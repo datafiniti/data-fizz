bd78f43dd19902853bca95945649388f5c145a83
jsandfb <james@destructoid.com>
2014-06-26 19:47:57 -0500

    Removed log directory and added download path

--- HereInMyCar/ConfigVars.php
+++ HereInMyCar/ConfigVars.php
@@ -1,7 +1,7 @@
 <?php
 // Application variables - change with caution
 define('HOME_PATH', dirname(__FILE__));
-define('LOG_PATH', HOME_PATH . '/logs');
+define('DOWNLOAD_PATH', HOME_PATH . '/downloads');
 
 // User defined variables below
 define('DB_USER', 'datafiniti');
