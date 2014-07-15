07e906af9e0daa50af0c3f3463fe51e475183b5d
Ashley McKemie <ashley.mckemie@gmail.com>
2014-07-15 16:01:53 -0500

    Complete title and author parsing

--- lib/entities/amazon_crawler.rb
+++ lib/entities/amazon_crawler.rb
@@ -1,9 +1,31 @@
 require 'nokogiri' 
+require 'pry'
 
 module DF
   class AmazonCrawler < WebCrawler
+  	attr_reader :title, :author, :isbn, :weight, :price, :page
   	def initialize(source)
   	  super(source)
+  	  @page = self.create_html_object
+  	  @title = nil
+  	  @author = nil
+  	  @isbn = nil
+  	  @weight = nil
+  	  @price = nil
+  	end
+
+  	# Note: This could also be achieved by splitting the title_header into an array at colons, deleting the last 3 elements in the array and parsing through the remaining
+  	def parse_title
+  	  title_header = @page.css('title').text.match(/(.\W)\d+(: Amazon.com: Books)$/).pre_match
+  	  matched = title_header.match(/\w+\s\w+\z/)
+  	  @author = matched.to_s
+  	  @title = matched.pre_match.slice(0..-3)
+  	end
+
+  	def parse_price
+  	end
+
+  	def parse_product_info
   	end
   end
 end
--- spec/entities/amazon_crawler_spec.rb
+++ spec/entities/amazon_crawler_spec.rb
@@ -3,16 +3,34 @@ require 'nokogiri'
 require 'open-uri'
 
 describe DF::AmazonCrawler do 
-  let(:source_1) {'./AllMyBooksArePacked/data/book10.html'}
-  let(:html_source) {DF::WebCrawler.new(source_1)}
+  before(:each) do 
+    source_1 = './AllMyBooksArePacked/data/book10.html'
+  	@html_source = DF::AmazonCrawler.new(source_1)
+  end
+  
+  describe 'initialize' do 
+  	it 'inherits the initialize method from the WebCrawler parent' do 
+  	  expect(@html_source.source).to eq('./AllMyBooksArePacked/data/book10.html')
+    end
 
-  it 'inherits the initialize method from the WebCrawler parent' do 
-  	page = html_source
-  	expect(page.source).to eq('./AllMyBooksArePacked/data/book10.html')
+    it 'initializes with a page attribute that is set to the Nokogiri HTML object' do
+      expect(@html_source.page.class).to eq(Nokogiri::HTML::Document)
+    end
+ 
+    it 'initializes with nil values for author, title, price, weight, and isbn' do 
+      expect(@html_source.title).to eq(nil)
+      expect(@html_source.author).to eq(nil)
+      expect(@html_source.isbn).to eq(nil)
+      expect(@html_source.price).to eq(nil)
+      expect(@html_source.weight).to eq(nil)
+    end
   end
 
-  it 'inherits the create a HTML Doc object method from WebCrawler parent' do 
-  	page = html_source.create_html_object
-  	expect(page.class).to eq(Nokogiri::HTML::Document)
+  describe 'parse_title' do 
+  	it "parses out the title and author of a book, storing them as instance variables" do 
+  	  @html_source.parse_title
+  	  expect(@html_source.title).to eq("The Ocean at the End of the Lane: A Novel")
+  	  expect(@html_source.author).to eq("Neil Gaiman")
+  	end
   end
 end
