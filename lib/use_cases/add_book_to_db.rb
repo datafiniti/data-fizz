c0ab2e366dce14885673972800456b6cf3c980ec
Ashley McKemie <ashley.mckemie@gmail.com>
2014-07-16 22:37:30 -0500

    Update get_shipping_box_id method to convert inputted weight to float. Complete adding book to shipping box in command

--- lib/use_cases/add_book_to_db.rb
+++ lib/use_cases/add_book_to_db.rb
@@ -8,19 +8,22 @@ module DF
       b_price = book.parse_book_price
       b_weight = book.parse_book_weight
       b_isbn = book.parse_book_isbn
+      b_box = get_shipping_box_id(b_weight) if b_weight
 
       if b_title && b_author && b_price && b_weight && b_isbn
       	return success :book => DF.db.create_book_product(title: b_title, 
       													author: b_author, 
       													price: b_price, 
       													shipping_weight: b_weight, 
-      													isbn10: b_isbn)
+      													isbn10: b_isbn,
+      													shipping_boxes_id: b_box)
       else 
       	return failure("Improper information.")
       end
     end
 
     def get_shipping_box_id(weight)
+      weight = weight.delete!("pounds").to_f
       new_weight = 10.00 - weight
       result = DF.db.get_box_by_weight(new_weight) 
       if result
--- spec/use_cases/add_book_to_db_spec.rb
+++ spec/use_cases/add_book_to_db_spec.rb
@@ -6,28 +6,43 @@ describe DF::AddBookToDatabase do
   	db.clear_tables
   end
 
-  it "adds a book's title, author, price, shipping weight, and isbn10 to the database" do
-  	book = DF::AmazonCrawler.new('./AllMyBooksArePacked/data/book5.html')
-  	result = subject.run(book)
-  	expect(result.success?).to eq(true)
-  	expect(result.book.title).to eq("The English Girl: A Novel (Gabriel Allon) [Hardcover]")
-  end
+  describe 'run' do 
+  	let(:book) {DF::AmazonCrawler.new('./AllMyBooksArePacked/data/book5.html')}
+	it "adds a book's title, author, price, shipping weight, and isbn10 to the database" do
+	  result = subject.run(book)
+	  expect(result.success?).to eq(true)
+	  expect(result.book.title).to eq("The English Girl: A Novel (Gabriel Allon) [Hardcover]")
+	end
 
-  it 'returns an error if improper information is gathered from the book' do 
-  	test = DF::AmazonCrawler.new('http://www.amazon.com/Runaway-Bride-Joan-Cusack/dp/B00AEBB8NS/ref=sr_1_1?s=movies-tv&ie=UTF8&qid=1405527733&sr=1-1&keywords=runaway+bride')
- 	result = subject.run(test)
- 	expect(result.success?).to eq(false)
- 	expect(result.error).to eq("Improper information.")
+	it 'adds the book to a new shipping box if the weight exceeds any available boxes' do
+	  result = subject.run(book)
+	  expect(result.success?).to eq(true)
+	  expect(result.book.shipping_boxes_id).to_not be_nil
+	end
+
+	it 'adds the book to a shipping box with enough available space (less than 10)' do
+  	  box = db.create_shipping_box(total_weight: 4.1)
+	  result = subject.run(book)
+	  expect(result.success?).to eq(true)
+	  expect(result.book.shipping_boxes_id).to eq(box.id)
+	end
+
+	it 'returns an error if improper information is gathered from the book' do 
+	  test = DF::AmazonCrawler.new('http://www.amazon.com/Runaway-Bride-Joan-Cusack/dp/B00AEBB8NS/ref=sr_1_1?s=movies-tv&ie=UTF8&qid=1405527733&sr=1-1&keywords=runaway+bride')
+	  result = subject.run(test)
+	  expect(result.success?).to eq(false)
+	  expect(result.error).to eq("Improper information.")
+	end
   end
 
   describe 'place_in_box' do 
   	it "creates a box record if there is no box with a weight less than the inputted weight and returns that box's id" do 
-  	  expect(subject.get_shipping_box_id(5.0)).to_not be_nil
+  	  expect(subject.get_shipping_box_id("5.0 pounds")).to_not be_nil
   	end
 
   	it 'returns the id of the box that the item should be put in if there db query does not return nil' do 
   	  box = db.create_shipping_box(total_weight: 4.1)
-  	  expect(subject.get_shipping_box_id(5.9)).to eq(box.id)
+  	  expect(subject.get_shipping_box_id("5.9 pounds")).to eq(box.id)
   	end
   end
 
