d7169c65b70ec7427283f899eddd93919d942e11
Katrina Theodosopoulos <katrinaelaine6@gmail.com>
2014-07-21 18:47:06 -0500

    Pass initial tests for Bin and Ship classes.

--- AllMyBooksArePacked/parse_books.rb
+++ AllMyBooksArePacked/parse_books.rb
@@ -3,86 +3,89 @@ require 'nikkou'
 require 'json'
 
 
-class Book
+module ParsePages
 
-  attr_reader :page, :title, :author, :price, :shipping_weight, :weight, :isbn
+  class Book
 
-  def initialize(filename)
-    @page = Nokogiri::HTML(open("data/#{filename}"))
-  end
+    attr_reader :page, :title, :author, :price, :shipping_weight, :weight, :isbn
 
-  def title
-    @title = @page.css("#btAsinTitle").text
-    @title.slice! " [Hardcover]"
-    @title.slice! " [Paperback]"
-    @title
-  end
-
-  def author
-    @author = @page.css(".buying > span > a")[0].text
-    @author
-  end
-
-  def price
-    @price = @page.css(".priceLarge").text
-    @price = @page.css(".rentPrice")[0].text if @price == ""
-    @price += " USD"
-    @price
-  end
-
-  def shipping_weight
-    @shipping_weight = @page.search('li').text_includes('pounds').text.match(/[0-9].[0-9]\s[a-zA-Z]*/).to_s
-    @shipping_weight
-  end
-
-  def weight
-    @weight = @page.search('li').text_includes('pounds').text.match(/[0-9].[0-9]/).to_s.to_f
-    @weight
-  end
-
-  def isbn
-    @isbn = @page.search('li').text_includes('ISBN-10').text.gsub!(/ISBN-10: /, "").to_s.to_i
-    @isbn
-  end
-
-end
-
-class Ship
-
-  attr_accessor :books, :boxes
-
-  def initialize(*books, boxes)
-    @books = books
-    @boxes = boxes
-  end
-
-  def calculate_boxes
-    max_weight = 10
-    total_weight = 0
-    @books.each do |book|
-      total_weight += book.weight
+    def initialize(filename)
+      @page = Nokogiri::HTML(open("data/#{filename}"))
     end
 
-    if total_weight < max_weight
-      num_boxes = 1
-    else
+    def title
+      @title = @page.css("#btAsinTitle").text
+      @title.slice! " [Hardcover]"
+      @title.slice! " [Paperback]"
+      @title
     end
+
+    def author
+      @author = @page.css(".buying > span > a")[0].text
+      @author
+    end
+
+    def price
+      @price = @page.css(".priceLarge").text
+      @price = @page.css(".rentPrice")[0].text if @price == ""
+      @price += " USD"
+      @price
+    end
+
+    def shipping_weight
+      @shipping_weight = @page.search('li').text_includes('pounds').text.match(/[0-9].[0-9]\s[a-zA-Z]*/).to_s
+      @shipping_weight
+    end
+
+    def weight
+      @weight = @page.search('li').text_includes('pounds').text.match(/[0-9].[0-9]/).to_s.to_f
+      @weight
+    end
+
+    def isbn
+      @isbn = @page.search('li').text_includes('ISBN-10').text.gsub!(/ISBN-10: /, "").to_s.to_i
+      @isbn
+    end
+
   end
 
-end
+  class Ship
 
-class Bin
+    attr_accessor :items, :max_weight
 
-  attr_accessor :total_weight, :contents
+    def initialize(items, max_weight)
+      @items = items
+      @max_weight = max_weight
+    end
 
-  def initialize
-    @total_weight = 0
-    @contents = []
+    def calculate_boxes
+      total_weight = 0
+      @items.each do |item|
+        total_weight += item.weight
+      end
+
+      if total_weight < max_weight
+        num_boxes = 1
+      else
+      end
+    end
+
   end
 
-  def add_content(content)
-    total_weight += content.weight
-    @contents << item
+  class Bin
+
+    attr_accessor :total_weight, :contents
+
+    def initialize
+      @total_weight = 0
+      @contents = []
+    end
+
+    def add_content(content)
+      @total_weight += content.weight
+      @contents << content
+    end
+
   end
 
 end
