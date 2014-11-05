0f3352c31dd418d0d2aff767130b59da46f84759
Joseph Tingsanchali <charnwoot@gmail.com>
2014-11-04 21:59:54 -0600

    Move comments before tests

--- spec/test_package.rb
+++ spec/test_package.rb
@@ -21,25 +21,27 @@ class Tests < Minitest::Unit::TestCase
     assert_equal 2, number_of_packages(list)
   end
  
+  # Confirm total weight has not changed
   def test_4
     list = [9.0, 5.0, 1.0, 5.0, 1.0, 1.0, 5.0, 7.0, 1.0, 5.0].map { |i| Package.new(i) }
     sum = 0.0
     assert_equal 4, number_of_packages(list)
 
-    # Confirm total weight has not changed
     assert_equal 40.0, list.inject(0){ |sum, x| sum + x.weight}
   end
 
+  # Confirm total weight has not changed
   def test_5
     list = %w(9.4 8.8 7.8 7.6 6.4 5.6 4.9 4.4 4.1 3.6 3.6 3.2 2.8 2.6 2.2 2.2 1.4 1.4 1.4 1.2)
     list = list.map(&:to_f).shuffle.map { |i| Package.new(i) }
     assert_equal 9, number_of_packages(list)
     
-    # Confirm total weight has not changed
     sum = 0.0
     assert_equal 84.6, list.inject(0){ |sum, x| sum + x.weight}.round(2)
   end
 
+  # Verify that total boxes meet limit of MAX_WEIGHT
+  # and verify each box total
   def test_6
     list = %w(9.4 8.8 7.8 7.6 6.4 5.6 4.9 4.4 4.1 3.6 3.6 3.2 2.8 2.6 2.2 2.2 1.4 1.4 1.4 1.2)
     list = list.map(&:to_f).shuffle.map { |i| Package.new(i) }
