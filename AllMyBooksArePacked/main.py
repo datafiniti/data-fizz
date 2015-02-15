6b224f0969a2da1c230828492d2e6b61f5057dc9
Jeffrey D <literallyelvis@gmail.com>
2015-02-15 12:19:44 -0600

    Quick prototype of data retrieval only.

---
+++ AllMyBooksArePacked/main.py
@@ -0,0 +1,56 @@
+from bs4 import BeautifulSoup
+import os, time, glob
+
+start = time.time()
+results = []
+os.chdir("data")
+
+
+class Book:
+	def __init__(self, title, author, price, ISBN, weight):
+		self.title = title
+		self.author = author
+		self.price = price
+		self.ISBN = ISBN
+		self.weight = weight
+
+
+	def __repr__(self):
+		return "{0}: {1} by {2}, {3}. {4}".format(self.ISBN, self.title, self.author, self.price, self.weight)
+
+
+def extract_content(tag):
+	start = tag.find(">") + 1
+	end = tag.find("<", start)
+	return tag[start:end].strip()
+
+for file in glob.glob("*.html"):
+	working_file = open(file, "r", encoding="ISO-8859-1")
+	soup = BeautifulSoup(working_file.read())
+
+	title = extract_content(str(soup.find(id="btAsinTitle")))
+
+	author_data = str(soup.findAll("meta", {"name": "keywords"})[0])
+	author = author_data[author_data.find('"') + 1:author_data.find(",")]
+
+	price_data = soup.findAll("span", {"class": "bb_price"})[0]
+	price = extract_content(str(price_data))
+
+	remaining_data = str(soup.findAll("td", {"class": "bucket"})[0])
+	soup = BeautifulSoup(remaining_data)
+	remaining_data = soup.findAll("li")
+
+	for data in remaining_data:
+		if "ISBN-10" in str(data):
+			ISBN = str(data)
+		elif "Weight" in str(data):
+			weight = str(data)
+
+	ISBN = ISBN[ISBN.find("</b> ") + 5:ISBN.find("</li>")]
+	weight = weight[weight.find("</b> ") + 5:weight.find(" (")]
+
+	new_book = Book(title, author, price, ISBN, weight)
+	results.append(new_book)
+
+print(results)
+print(time.time() - start)
