c1ce464219c2eb6188a8681be7278ed724c72035
Jeffrey D <literallyelvis@gmail.com>
2015-02-25 21:47:42 -0600

    Functions fully fleshed out.

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class BookParser(HTMLParser):
     author = None
     title = None
     price = None
-    ISBN_10 = None
+    isbn = None
     weight = None
 
     def handle_starttag(self, tag, attrs):
@@ -20,9 +20,8 @@ class BookParser(HTMLParser):
             for attr in attrs:
                 if "btAsinTitle" in attr:  # handles book title
                     self.data_inbound = "title"
-
-        if tag == "b" and attrs and "priceLarge" in attrs[0]:
-            self.data_inbound = "price"
+                if "bb_price" in attr:
+                    self.data_inbound = "price"
 
     def handle_data(self, data):
         if self.data_inbound == "title":
@@ -32,7 +31,7 @@ class BookParser(HTMLParser):
             self.price = data.strip()
 
         elif self.data_inbound == "ISBN":
-            self.ISBN_10 = data.strip()
+            self.isbn = data.strip()
 
         elif self.data_inbound == "weight":
             self.weight = data.replace(" (", "").strip()
@@ -46,16 +45,16 @@ class BookParser(HTMLParser):
             self.data_inbound = "weight"
 
     def finalize(self, result_list):
-        book = Book(self.title, self.author, self.price, self.ISBN_10, self.weight)
+        book = Book(self.title, self.author, self.price, self.isbn, self.weight)
         result_list.append(book)
 
 
 class Book:
-    def __init__(self, title, author, price, ISBN, weight):
+    def __init__(self, title, author, price, isbn, weight):
         self.title = title
         self.author = author
         self.price = price
-        self.ISBN = ISBN
+        self.isbn = isbn
         self.weight = weight
 
     def get_numeric_weight(self):
@@ -66,7 +65,10 @@ class Box:
     def __init__(self, box_id, books):
         self.id = box_id
         self.contents = [book for book in books]
-        self.total_weight = sum(book.get_numeric_weight() for book in books)
+        self.total_weight = round(sum(book.get_numeric_weight() for book in books), 1)
+
+    def remove_from_box(self, book):
+        self.contents.remove(book)
 
 
 class Shipment:
@@ -80,6 +82,8 @@ class Shipment:
     def create_from(self, books, n=None):
         books = sorted(books, key=lambda book: book.weight, reverse=True)
         total_weight = sum(book.get_numeric_weight() for book in books)
+        box = []
+        books_used = []
 
         if n and n > len(books):
             raise ValueError("N is larger than the number of books")
@@ -87,25 +91,31 @@ class Shipment:
             raise ValueError("N is so small that at least one box would exceed 10 pounds.")
 
         while books:
-            box = []
-            box_id = 0
+            if box:
+                final_box = Box(self.box_id, box)
+                self.add_box_to_shipment(final_box)
+                self.box_id += 1
+                box = []
+            else:
+                box = []
+                books_used = []
+
+            books = [book for book in books if book not in books_used]
+
             for book in books:
                 current_weight = sum(book.get_numeric_weight() for book in box)
-                if 10 > (book.get_numeric_weight() + current_weight):
+                if 10 >= (book.get_numeric_weight() + current_weight):
                     box.append(book)
-                    books.remove(book)
-                else:
-                    final_box = Box(self.box_id, box)
-                    self.add_box_to_shipment(final_box)
-                    box = []
-                    self.box_id += 1
+                    books_used.append(book)
 
-        # n = 20
         if n and n > len(self.boxes):
             while n > len(self.boxes):
                 for box in self.boxes:
+                    if n == len(self.boxes):
+                        break
                     if len(box.contents) > 1:
-                        new_box = Box(self.box_id, [box.contents.pop()])
+                        new_box = Box(self.box_id, [box.contents[0]])
+                        box.remove_from_box(box.contents[0])
                         self.add_box_to_shipment(new_box)
                         self.box_id += 1
 
