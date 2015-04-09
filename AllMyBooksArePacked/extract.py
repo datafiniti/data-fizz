891d7bec12f90e35f782e7164d0728b948d44339
sanketjpatel <sanketpatel.301090@gmail.com>
2015-04-09 02:54:55 -0500

    Complete parsing all methods

--- AllMyBooksArePacked/extract.py
+++ AllMyBooksArePacked/extract.py
@@ -3,10 +3,12 @@ from bs4 import BeautifulSoup
 parsed_html = BeautifulSoup(open('data/book16.html'), 'html.parser')
 
 def find_title_author(parsed_html):
+    """Find title_author element"""
     return parsed_html.find('span', attrs={'id':'btAsinTitle'}).parent.parent
 
 def find_product_details(parsed_html):
-    return parsed_html.find('table', attrs={'id':'productDetailsTable'})
+    """Find product_details element"""
+    return parsed_html.find('table', attrs={'id':'productDetailsTable'}).tr.td.div.ul
 
 def find_title(title_author):
     return str(title_author.h1.span.contents[0])
@@ -18,11 +20,11 @@ def find_price(parsed_html):
     return float(str(parsed_html.find('span', attrs={'id':'actualPriceValue'}).b.text)[1:])
    
 def find_weight(product_details):
-    pass
-    return product_details.contents
+    return float(str(product_details.contents[13].contents[1][:-8]).strip())
 
 def find_isbn(parsed_html):
-    pass
+    """Returns a string to preserve leading zeros"""
+    return str(product_details.contents[7].contents[1]).strip()
 
 title_author = find_title_author(parsed_html)
 product_details = find_product_details(parsed_html)
@@ -38,3 +40,4 @@ print 'Title:', title
 print 'Author:', author
 print 'Price:', price
 print 'Weight:', shipping_weight
+print 'ISBN:', isbn_10
