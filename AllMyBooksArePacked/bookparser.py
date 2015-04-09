a98b94ddc8514317e4e5f1aac4824b6832304bc9
sanketjpatel <sanketpatel.301090@gmail.com>
2015-04-09 05:29:02 -0500

    Parse functions debugged

--- AllMyBooksArePacked/bookparser.py
+++ AllMyBooksArePacked/bookparser.py
@@ -16,33 +16,24 @@ def find_author(title_author_element):
     return str(title_author_element.contents[3].a.text)
 
 def find_price(parsed_html):
-    return float(str(parsed_html.find('span', attrs={'id':'actualPriceValue'}).b.text)[1:])
+    return float(str(parsed_html.find('span', attrs={'class':'bb_price'}).text).strip()[1:].replace(',',''))
    
 def find_weight(product_details):
-    pass
-    #return float(str(product_details_element.contents[13].contents[1][:-8]).strip())
+    """
+    Returns weight in pounds
+    
+    We might be reiterating through product_details,
+    but this will help in case we need to find weights
+    in a different context for scraping data.
+    
+    Can change this to improve speed.
+    """
+    for listItem in product_details:
+        if listItem.b.text == 'Shipping Weight:':
+            return float(listItem.contents[1][1:-9])
 
 def find_isbn(product_details):
     """Returns a string to preserve leading zeros"""    
     for listItem in product_details:
         if listItem.b.text == 'ISBN-10:':
             return str(listItem.contents[1]).strip()
-    
-
-# Parse HTML file
-parsed_html = BeautifulSoup(open('data/book%d.html' % 6), 'html.parser')
-
-# Extract important objects
-title_author = find_title_author(parsed_html)
-product_details = find_product_details(parsed_html)
-
-# title = title_author.h1.span.text #if you also want [Hardcover] at the end
-title = find_title(title_author)
-author = find_author(title_author)
-price = find_price(parsed_html)
-shipping_weight = find_weight(product_details)
-isbn_10 = find_isbn(product_details)
-
-book = Book(title, author, price, shipping_weight, isbn_10)
-book.print_details()
-print '\n'
