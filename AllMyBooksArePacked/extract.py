f4f3081ea8f05d70c09ebeab0050bd2a13c860aa
sanketjpatel <sanketpatel.301090@gmail.com>
2015-04-09 01:40:38 -0500

    method naming

--- AllMyBooksArePacked/extract.py
+++ AllMyBooksArePacked/extract.py
@@ -2,10 +2,10 @@ from bs4 import BeautifulSoup
 
 parsed_html = BeautifulSoup(open('data/book16.html'), 'html.parser')
 
-def findTitleAuthorElement(parsed_html):
+def find_title_author(parsed_html):
     return parsed_html.find('span', attrs={'id':'btAsinTitle'}).parent.parent
 
-title_author = findTitleAuthorElement(parsed_html)
+title_author = find_title_author(parsed_html)
 
 title = title_author.h1.span
 print title.text
