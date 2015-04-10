dd7a83cd8a9a8570911029dc22c306fe3dd9e926
sanketjpatel <sanketpatel.301090@gmail.com>
2015-04-09 22:27:32 -0500

    Install dependencies

--- AllMyBooksArePacked/build.py
+++ AllMyBooksArePacked/build.py
@@ -1,5 +1,10 @@
-from pybuilder.core import use_plugin
+from pybuilder.core import init, use_plugin
 
 use_plugin("python.core")
+use_plugin("python.install_dependencies")
 
 default_task = "publish"
+
+@init
+def initialize(project):
+	project.build_depends_on('BeautifulSoup')
