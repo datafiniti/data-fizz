7b913e8aed9422388c83a2417d2c71a9f6afe503
Stephen Clem <stevebclem@gmail.com>
2015-05-02 16:13:20 -0700

    cleaned up put statements

--- AllMyBooksArePacked/book_packer_api/db/seeds.rb
+++ AllMyBooksArePacked/book_packer_api/db/seeds.rb
@@ -2,54 +2,45 @@ require_relative '../app/class_helpers/web_scrapinizor'
 require_relative '../app/class_helpers/book_parser'
 require_relative '../app/class_helpers/create_object'
 
+def scrape_and_seed_books
+  files_to_scrape = Dir.entries("./../data").select{|file| file.include?("html")}.map{|file| "./../data/#{file}"}
 
-files_to_scrape = Dir.entries("./../data").select{|file| file.include?("html")}.map{|file| "./../data/#{file}"}
+  files_to_scrape.each do |file|
 
-files_to_scrape.each do |file|
+    scraped_file = Scrape::AmazonBook.new(file).nokogiri_scrape
 
-  scraped_file = Scrape::AmazonBook.new(file).nokogiri_scrape
+    book_info = Parser::AmazonBookParser.new(scraped_file)
 
-  book_info = Parser::AmazonBookParser.new(scraped_file)
-
-  Create::AmazonBook.new(book_info).create_object
+    Create::AmazonBook.new(book_info).create_object
+  end
 end
 
 def box_packing
 
   @warehouse_guy = Create::BookPackagePacker.new
   @warehouse_guy.begin_boxing
-  # p Book.all
 
   Book.order(shipping_weight: :desc).each do |book|
       @book = book
       $book_weight = @book.shipping_weight.match((/\d+[,.]\d+/)).to_s.to_f
-      p "Book #{book.id} shipping weight: #{$book_weight}"
 
     Box.all.each do |box|
+
       $box = box
       $box_total_weight = $box.totalWeight.match((/\d+[,.]\d+/)).to_s.to_f
-      p "Box #{$box.id} total weight right now: #{$box_total_weight}"
 
       if (@warehouse_guy.weigh_box_with_book) <= 10
         @warehouse_guy.add_item_to_box(@book)
-        p "Putting book #{@book.id} into  box #{$box.id}"
         break
       elsif Box.find($box.id).next.empty?
-        p "current book weight #{$book_weight}"
-        p "Book #{@book.id} could fit in the next box"
         Box.create(totalWeight: "#{$book_weight} pounds", contents: [@book])
-        # p "now the box weight it #{new_box.shipping_weight}"
-
         break
-      else
-        p "Book #{book.id} is too heavy for box #{$box.id}"
-
       end
     end
-    puts
   end
 end
 
+scrape_and_seed_books
 box_packing
 
 
