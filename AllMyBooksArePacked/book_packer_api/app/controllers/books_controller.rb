cbe6540a934aad4c85c7bc682cd223cb47709e1a
Stephen Clem <stevebclem@gmail.com>
2015-05-02 14:05:30 -0700

    added book index and controller testing positive

--- AllMyBooksArePacked/book_packer_api/app/controllers/books_controller.rb
+++ AllMyBooksArePacked/book_packer_api/app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+
+  #route returns a json of all the books scraped
   def index
     @books = Book.all
     render json: @books, :except => [:id, :created_at, :updated_at]
--- AllMyBooksArePacked/book_packer_api/app/controllers/boxes_controller.rb
+++ AllMyBooksArePacked/book_packer_api/app/controllers/boxes_controller.rb
@@ -1,2 +1,10 @@
 class BoxesController < ApplicationController
+  def index
+    @boxes = Box.all
+    render json: Box.all.as_json(
+      :root => true,
+      :except => [:created_at, :updated_at]),
+
+    :except => [ :box_id, :created_at, :updated_at]
+  end
 end
