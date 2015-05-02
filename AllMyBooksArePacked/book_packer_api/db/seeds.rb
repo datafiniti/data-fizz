54ac99d808a59ce7a285de244747a67773b4b6a5
Stephen Clem <stevebclem@gmail.com>
2015-05-02 13:26:36 -0700

    Boom. looped through files and creates a book in our database

--- AllMyBooksArePacked/book_packer_api/db/seeds.rb
+++ AllMyBooksArePacked/book_packer_api/db/seeds.rb
@@ -1,12 +1,18 @@
 require_relative '../app/class_helpers/web_scrapinizor'
 require_relative '../app/class_helpers/book_parser'
-# require_relative '../app/class_helpers/create_record'
+require_relative '../app/class_helpers/create_object'
 
 
 files_to_scrape = Dir.entries("./../data").select{|file| file.include?("html")}.map{|file| "./../data/#{file}"}
 
 files_to_scrape.each do |file|
 
-  puts file
+  scraped_file = Scrape::AmazonBook.new(file).nokogiri_scrape
+
+  book_info = Parser::AmazonBookParser.new(scraped_file)
+
+  Create::AmazonBook.new(book_info).create_object
 
 end
+
+
