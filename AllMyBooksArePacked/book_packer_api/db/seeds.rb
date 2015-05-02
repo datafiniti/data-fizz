91492fecfa3d519d0d51f8bc6400cf26edb53dbe
Stephen Clem <stevebclem@gmail.com>
2015-05-02 13:10:40 -0700

    grabbed the whole file for scraping

--- AllMyBooksArePacked/book_packer_api/db/seeds.rb
+++ AllMyBooksArePacked/book_packer_api/db/seeds.rb
@@ -3,7 +3,7 @@ require_relative '../app/class_helpers/book_parser'
 # require_relative '../app/class_helpers/create_record'
 
 
-files_to_scrape = Dir.entries('./../data').select{|file| file.include?("html")}
+files_to_scrape = Dir.entries("./../data").select{|file| file.include?("html")}.map{|file| "./../data/#{file}"}
 
 files_to_scrape.each do |file|
 
