aad70797ce5013b13514593107e693e9485d76aa
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-05-14 13:36:26 -0500

    strips extra text from information for working HTML files. Next step: examine books 6, 12, and 20.

--- AllMyBooksArePacked/scripts/parser.rb
+++ AllMyBooksArePacked/scripts/parser.rb
@@ -4,22 +4,31 @@ require 'nokogiri'
 book_numbers = Array(1..20)
 
 book_numbers.each do |n|
-  path = "../data/book" + n.to_s + ".html"
+  unless n == 6 || n == 12 || n == 20
+    path = "../data/book" + n.to_s + ".html"
   
-  page = Nokogiri::HTML(open(path))
-
-  title = page.css("span[id=btAsinTitle]").text
-  author = page.css("div[class=buying] a")[2].text
-  price = page.css("span[id=actualPriceValue]").text
-  weight = page.css("div[id=detail-bullets]").css('li')[6].text
-  isbn = page.css("div[id=detail-bullets]").css('li')[3].text
+    page = Nokogiri::HTML(open(path))
+    
+    rawTitle = page.css("span[id=btAsinTitle]").text
+    title = rawTitle.slice(0...(rawTitle.index('[')))
+    
+    author = page.css("div[class=buying] a")[2].text
+    
+    price = page.css("span[id=actualPriceValue]").text
+    
+    
+    rawWeight = page.css("div[id=detail-bullets]").css('li')[6].text
+    weight = rawWeight.slice((rawWeight.index(":")+2)...(rawWeight.index(" pounds"))).to_f
+    
+    rawIsbn = page.css("div[id=detail-bullets]").css('li')[3].text
+    isbn = rawIsbn.split(": ")[1]
   
-  puts n
-  puts title #TODO take of hardcover text if possible
-  puts author
-  puts weight #TODO has extra text
-  puts isbn #TODO has extra text
-  puts " "
+    puts title
+    puts author
+    puts weight
+    puts isbn 
+    puts " "
+  end
 end
 
 
