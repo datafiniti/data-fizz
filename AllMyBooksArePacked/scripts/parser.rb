aba1e65cab12f7c6c7fc14a1232b4007158b10c3
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-05-14 13:05:12 -0500

    builds loop for data files; nokogiri script breaks for books 6, 12, and 20.

--- AllMyBooksArePacked/scripts/parser.rb
+++ AllMyBooksArePacked/scripts/parser.rb
@@ -1,16 +1,25 @@
 require 'rubygems'
 require 'nokogiri'         
 
-page = Nokogiri::HTML(open("../data/book2.html"))
+book_numbers = Array(1..20)
 
-title = page.css("span[id=btAsinTitle]").text
-author = page.css("div[class=buying]").css('a')[2].text
-price = page.css("span[id=actualPriceValue]").text
-weight = page.css("div[id=detail-bullets]").css('li')[6].text
-isbn = page.css("div[id=detail-bullets]").css('li')[3].text
+book_numbers.each do |n|
+  path = "../data/book" + n.to_s + ".html"
+  
+  page = Nokogiri::HTML(open(path))
 
-puts title #TODO take of hardcover text if possible
-puts author
-puts price
-puts weight #TODO has extra text
-puts isbn #TODO has extra text
+  title = page.css("span[id=btAsinTitle]").text
+  author = page.css("div[class=buying] a")[2].text
+  price = page.css("span[id=actualPriceValue]").text
+  weight = page.css("div[id=detail-bullets]").css('li')[6].text
+  isbn = page.css("div[id=detail-bullets]").css('li')[3].text
+  
+  puts n
+  puts title #TODO take of hardcover text if possible
+  puts author
+  puts weight #TODO has extra text
+  puts isbn #TODO has extra text
+  puts " "
+end
+
+
