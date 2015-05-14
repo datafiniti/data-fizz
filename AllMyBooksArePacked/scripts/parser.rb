28ac78e24bbc784cb1ba3a86d3a316e3c2222b53
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-05-14 17:08:25 -0500

    troubleshoot through the infinite loop issue - problem rests with duplicate entries and how the program currently deals with tracking which products have been boxed (do not use a utility array like is currently written in the code); also adds extension text file. Next steps: fix infinite loop; format collection of data differently; begin moving this work into classes

--- AllMyBooksArePacked/scripts/parser.rb
+++ AllMyBooksArePacked/scripts/parser.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
-require 'nokogiri'         
+require 'nokogiri'
+require 'pry'         
 
 # def scrape
 product_count = Array(1..20)
@@ -7,30 +8,27 @@ product_count = Array(1..20)
 product_data = [] #Array of arrays
 
 product_count.each do |n|
-  unless n == 6 || n == 12 || n == 20 #TODO figure out while scraping breaks for these entries; look into issues with scaling.
+  # unless n == 6 || n == 12 || n == 20 #TODO figure out while scraping breaks for these entries; look into issues with scaling.
     path = "../data/book" + n.to_s + ".html"
   
     page = Nokogiri::HTML(open(path))
     
     rawTitle = page.css("span[id=btAsinTitle]").text #TODO agnosticize 
-    title = rawTitle.slice(0...(rawTitle.index(' [')))
     
     author = page.css("div[class=buying] a")[2].text
     
     price = page.css("span[id=actualPriceValue]").text
     
     rawWeight = page.css("div[id=detail-bullets]").css('li')[6].text
-    weight = rawWeight.slice((rawWeight.index(":")+2)...(rawWeight.index(" pounds"))).to_f
     
     rawIsbn = page.css("div[id=detail-bullets]").css('li')[3].text
+    
+    binding.pry
+    
+    title = rawTitle.slice(0...(rawTitle.index(' [')))
+    weight = rawWeight.slice((rawWeight.index(":")+2)...(rawWeight.index(" pounds"))).to_f
     isbn = rawIsbn.split(": ")[1]
-  
-    # puts title
-    # puts author
-    # puts price
-    # puts weight
-    # puts isbn
-    # puts " "
+    
     
     info = [{weight: weight}, 
             { "title": title, 
@@ -40,41 +38,54 @@ product_count.each do |n|
             "isbn-10": isbn
             }]
                 
-    product_data << info
-  end
+    product_data << info #Array << Array
+  # end
 end
-
-# puts product_data[0]
+        
 # end
 
 
 # def box
 
-json = { 
-        "box": {
-          "id": 1, 
-          "totalWeight": 0, 
-          "contents": Array.new
-          }
-        }
-        
-l = product_data.length - 1
-puts l 
-for i in 0..l
-  product = product_data[i]
-  if product[0][:weight] + json[:box][:totalWeight] <= 10
-    json[:box][:totalWeight] += product[0][:weight]
-    json[:box][:contents] << product[1]
-    i += 1
-  else
-    i += 1
-  end
-end
-
-json[:box][:totalWeight] = json[:box][:totalWeight].to_s + " pounds"
-
-puts json
-
+# json = {} # Hash of :box => {} k,v pairs
+# boxed_products = [] #Utility variable for tracking which items have been boxed.
+#
+# box_no = 1 #iterator
+#
+# loop do
+#   box_info = {
+#               "id": box_no,
+#               "totalWeight": 0,
+#               "contents": Array.new #Array of Hashes, each hash is info about a specific product
+#               }
+#
+#   l = product_data.length - 1
+#   for product_index in 0..l #iterator
+#     product = product_data[product_index]
+#     if product[0][:weight] + box_info[:totalWeight] <= 10 && !boxed_products.include?(product)
+#       box_info[:totalWeight] += product[0][:weight]
+#       box_info[:contents] << product[1] #product info hash (excludes weight measurement)
+#       boxed_products << product
+#       product_index += 1
+#     else
+#       product_index += 1
+#     end
+#   end
+#
+#   json[:box] = box_info #TODO problem here with how we are setting up the hash
+#   json[:box][:totalWeight] = json[:box][:totalWeight].to_s + " pounds"
+#
+#   binding.pry
+#
+#   box_no += 1
+#
+#   if product_data.length == boxed_products.length
+#     break
+#   end
 # end
-
-
+#
+# puts json
+#
+# # end
+#
+#
