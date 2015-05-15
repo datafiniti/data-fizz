053b3aedda3988e462df02e128e876dcc3c89b45
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-05-14 21:31:54 -0500

    rewrites boxing loop format that will eventually be passed to a json doc

--- AllMyBooksArePacked/scripts/parser.rb
+++ AllMyBooksArePacked/scripts/parser.rb
@@ -8,7 +8,7 @@ product_count = Array(1..20)
 product_data = [] #Array of arrays
 
 product_count.each do |n|
-  # unless n == 6 || n == 12 || n == 20 #TODO figure out while scraping breaks for these entries; look into issues with scaling.
+  unless n == 6 || n == 12 || n == 20 #TODO figure out while scraping breaks for these entries; look into issues with scaling.
     path = "../data/book" + n.to_s + ".html"
   
     page = Nokogiri::HTML(open(path))
@@ -22,9 +22,7 @@ product_count.each do |n|
     rawWeight = page.css("div[id=detail-bullets]").css('li')[6].text
     
     rawIsbn = page.css("div[id=detail-bullets]").css('li')[3].text
-    
-    binding.pry
-    
+        
     title = rawTitle.slice(0...(rawTitle.index(' [')))
     weight = rawWeight.slice((rawWeight.index(":")+2)...(rawWeight.index(" pounds"))).to_f
     isbn = rawIsbn.split(": ")[1]
@@ -39,7 +37,7 @@ product_count.each do |n|
             }]
                 
     product_data << info #Array << Array
-  # end
+  end
 end
         
 # end
@@ -47,45 +45,38 @@ end
 
 # def box
 
-# json = {} # Hash of :box => {} k,v pairs
-# boxed_products = [] #Utility variable for tracking which items have been boxed.
-#
-# box_no = 1 #iterator
-#
-# loop do
-#   box_info = {
-#               "id": box_no,
-#               "totalWeight": 0,
-#               "contents": Array.new #Array of Hashes, each hash is info about a specific product
-#               }
-#
-#   l = product_data.length - 1
-#   for product_index in 0..l #iterator
-#     product = product_data[product_index]
-#     if product[0][:weight] + box_info[:totalWeight] <= 10 && !boxed_products.include?(product)
-#       box_info[:totalWeight] += product[0][:weight]
-#       box_info[:contents] << product[1] #product info hash (excludes weight measurement)
-#       boxed_products << product
-#       product_index += 1
-#     else
-#       product_index += 1
-#     end
-#   end
-#
-#   json[:box] = box_info #TODO problem here with how we are setting up the hash
-#   json[:box][:totalWeight] = json[:box][:totalWeight].to_s + " pounds"
-#
-#   binding.pry
-#
-#   box_no += 1
-#
-#   if product_data.length == boxed_products.length
-#     break
-#   end
+json = [] # Array of hashes { box: {box_info} }
+
+box_no = 1 #iterator
+
+loop do
+  box_info = {
+              "id": box_no,
+              "totalWeight": 0,
+              "contents": Array.new #Array of Hashes, each hash is info about a specific product
+              }
+
+  product_data.each do |product|
+    if product[0][:weight] + box_info[:totalWeight] <= 10 
+      box_info[:totalWeight] += product[0][:weight]
+      box_info[:contents] << product[1] #product info hash (excludes weight measurement)
+      product_data.delete_at(product_data.index(product))
+    end
+  end
+
+  json << {box: box_info}
+
+  binding.pry
+
+  box_no += 1
+
+  if product_data.length < 1
+    break
+  end
+end
+
+puts json
+
 # end
-#
-# puts json
-#
-# # end
-#
-#
+
+
