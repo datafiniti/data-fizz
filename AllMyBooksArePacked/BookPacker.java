434eac6aa5a8b79dc3d989d94e4e8ed00964359e
Roger <li.rojie@gmail.com>
2015-06-08 21:59:03 -0400

    Code Cleaning
    Created helper methods to shorten buildBookArray, changed to camelCase,
    eliminated variables that could be accessed, eliminated variables that
    could just be nested within methods.

--- AllMyBooksArePacked/BookPacker.java
+++ AllMyBooksArePacked/BookPacker.java
@@ -7,119 +7,132 @@ import org.json.*;
 import org.apache.commons.io.*;
 
 public class BookPacker{
-  static Book[] book_array = new Book[20];
-  static JSONObject box_list = new JSONObject();
-  
+ 
   /**
-   * Creates book elements to be inserted in book_array, each one corresponding to a HTML file
+   * Creates book elements to be inserted in bookArray, each one corresponding to a HTML file
    */
-  public static void array_constructor(String path) throws IOException{
-    String url = path;
+  public static Book[] buildBookArray(String path) throws IOException{
+    Book[] bookArray = new Book[20];
     for (int i = 1; i <= 20; i++){
-      url = url + i + ".html";
       try{
-        Document html_file = Jsoup.parse(new File(url), "UTF-8");
-        //Obtain title information
-        Elements title_block = html_file.getElementsByClass("parseasinTitle");
-        String delim = "[\\>\\<]";
-        String[] title_tokens = title_block.html().split(delim);
-        String title = title_tokens[2];
-        Element body = html_file.body();
-        //Obtain author information
-        Elements author_block = body.getElementsByClass("buying").select("a[href]");
-        delim = "ordering";
-        String[] author_tokens = author_block.html().split(delim);
-        delim = "Details";
-        author_tokens = author_tokens[author_tokens.length - 1].split(delim);
-        String author = author_tokens[0].trim().replaceAll("\\n", " and "); //if more than one author, insert "and"
-        //Obtain price information
-        Elements price_block = body.getElementsByClass("bb_price");
-        delim = "[$]";
-        String[] price_tokens = price_block.html().split(delim);
-        String price = price_tokens[price_tokens.length - 1];
-        //Obtain ISBN information
-        Elements isbn_weight_block = body.getElementById("productDetailsTable").getElementsByClass("content");
-        delim = "ISBN-10:";
-        String[] isbn_token = isbn_weight_block.html().split(delim);
-        delim = "[\\>\\<]";
-        isbn_token = isbn_token[1].split(delim);
-        String isbn = isbn_token[2].trim();
-        //Obtain weight information
-        delim = "Shipping Weight:";
-        String[] weight_token = isbn_weight_block.html().split(delim);
-        delim = "[\\>\\<(]";
-        weight_token = weight_token[1].split(delim);
-        String weight = weight_token[2].trim();
-        delim = "[ ]";
-        weight_token = weight.split(delim);
-        weight = weight_token[0];
-        book_array[i-1] = new Book(title, author, price, weight, isbn);
+        Element body = Jsoup.parse(new File(path + i + ".html"), "UTF-8").body();
+        bookArray[i-1] = new Book(getTitle(body), getAuthor(body), getPrice(body), getWeight(body), getIsbn(body));
       }
       catch (IOException e){
         e.printStackTrace();
       }
-      url = path; //resets url back to folder path
     } 
+    return bookArray;
   }
   
-  public static void json_creator() throws JSONException{
-    JSONObject box = new JSONObject(); //box goes into box_array
-    JSONArray box_array = new JSONArray(); //box_array goes into box_list
-    int first_book = 0; //first book of each box
+  /**
+   * The following five helper methods obtain the appropriate field from the body
+   */
+  public static String getTitle(Element body){ 
+    Elements titleBlock = body.getElementsByClass("parseasinTitle");
+    String delim = "[\\>\\<]";
+    String[] titleTokens = titleBlock.html().split(delim);
+    return titleTokens[2];
+  }
+  
+  public static String getAuthor(Element body){ 
+    Elements authorBlock = body.getElementsByClass("buying").select("a[href]");
+    String delim = "ordering";
+    String[] authorTokens = authorBlock.html().split(delim);
+    delim = "Details";
+    authorTokens = authorTokens[authorTokens.length - 1].split(delim);
+    return authorTokens[0].trim().replaceAll("\\n", " and "); //if more than one author, insert "and"
+  }
+  
+  public static String getPrice(Element body){ 
+    Elements priceBlock = body.getElementsByClass("bb_price");
+    String delim = "[$]";
+    String[] priceTokens = priceBlock.html().split(delim);
+    return priceTokens[priceTokens.length - 1];
+  }
+  
+  public static String getIsbn(Element body){ 
+    Elements isbnBlock = body.getElementById("productDetailsTable").getElementsByClass("content");
+    String delim = "ISBN-10:";
+    String[] isbnToken = isbnBlock.html().split(delim);
+    delim = "[\\>\\<]";
+    isbnToken = isbnToken[1].split(delim);
+    return isbnToken[2].trim(); 
+  }
+  
+  public static String getWeight(Element body){ 
+    Elements weightBlock = body.getElementById("productDetailsTable").getElementsByClass("content");
+    String  delim = "Shipping Weight:";
+    String[] weightToken = weightBlock.html().split(delim);
+    delim = "[\\>\\<(]";
+    weightToken = weightToken[1].split(delim);
+    String weight = weightToken[2].trim();
+    delim = "[ ]";
+    weightToken = weight.split(delim);
+    return weightToken[0];
+  }
+  
+  public static JSONObject buildDataExport(Book[] bookArray) throws JSONException{
+    JSONObject boxList = new JSONObject();
+    JSONObject box = new JSONObject(); //box goes into boxArray
+    JSONArray boxArray = new JSONArray(); //boxArray goes into boxList
+    int firstBook = 0; //first book of each box
     int id = 1; //box id
-    double total_weight = 0; //total weight of all the books in the box
+    double totalWeight = 0; //total weight of all the books in the box
     for (int i = 0; i < 20; i++){
-      if (total_weight + Double.parseDouble(book_array[i].weight) < 10){ //add book to box if it'll fit
-        total_weight += Double.parseDouble(book_array[i].weight); 
+      if (totalWeight + Double.parseDouble(bookArray[i].weight) < 10){ //add book to box if it'll fit
+        totalWeight += Double.parseDouble(bookArray[i].weight); 
       }
       else{ //otherwise close the box, and compile JSON information
-        box.put("totalWeight", total_weight);
+        box.put("totalWeight", totalWeight);
         box.put("id", id);
-        JSONArray json_book_array = new JSONArray(); //json_book_array goes into box
-        for (int j = first_book; j < i; j++){ //for every book in the box
-          JSONObject array_element = new JSONObject(); //array_element goes into json_book_array
-          array_element.put("Title", book_array[j].title);
-          array_element.put("Author", book_array[j].author);
-          array_element.put("Price", "$" + book_array[j].price);
-          array_element.put("ISBN-10", book_array[j].isbn);
-          array_element.put("Weight", book_array[j].weight + " lbs");
-          json_book_array.put(array_element);
+        JSONArray jsonBookArray = new JSONArray(); //jsonBookArray goes into box
+        for (int j = firstBook; j < i; j++){ //for every book in the box
+          JSONObject arrayElement = new JSONObject(); //arrayElement goes into jsonBookArray
+          arrayElement.put("Title", bookArray[j].title);
+          arrayElement.put("Author", bookArray[j].author);
+          arrayElement.put("Price", "$" + bookArray[j].price);
+          arrayElement.put("ISBN-10", bookArray[j].isbn);
+          arrayElement.put("Weight", bookArray[j].weight + " lbs");
+          jsonBookArray.put(arrayElement);
         }
-        box.put("content", json_book_array);
-        box_array.put(box);
+        box.put("content", jsonBookArray);
+        boxArray.put(box);
         box = new JSONObject(); //reset the box
         id++; //new box id
-        total_weight = Double.parseDouble(book_array[i].weight); //the book that wasn't inserted on this iteration is in the next box
-        first_book = i; //change where the first book in the new box index value is
+        totalWeight = Double.parseDouble(bookArray[i].weight); //the book that wasn't inserted on this iteration is in the next box
+        firstBook = i; //change where the first book in the new box index value is
       }
     }
     //Edge condition: last book doesn't "close" the box from previous for loop, so repeat code in order to satisfy this
-    box.put("totalWeight", total_weight);
+    box.put("totalWeight", totalWeight);
     box.put("id", id);
-    JSONArray json_book_array = new JSONArray();
-    for (int i = first_book; i < 20; i++){
-      JSONObject array_element = new JSONObject();
-      array_element.put("Title", book_array[i].title);
-      array_element.put("Author", book_array[i].author);
-      array_element.put("Price", "$" + book_array[i].price);
-      array_element.put("ISBN-10", book_array[i].isbn);
-      array_element.put("Weight", book_array[i].weight + " lbs");
-      json_book_array.put(array_element);
+    JSONArray jsonBookArray = new JSONArray();
+    for (int i = firstBook; i < 20; i++){
+      JSONObject arrayElement = new JSONObject();
+      arrayElement.put("Title", bookArray[i].title);
+      arrayElement.put("Author", bookArray[i].author);
+      arrayElement.put("Price", "$" + bookArray[i].price);
+      arrayElement.put("ISBN-10", bookArray[i].isbn);
+      arrayElement.put("Weight", bookArray[i].weight + " lbs");
+      jsonBookArray.put(arrayElement);
     }
-    box.put("content", json_book_array);
-    box_array.put(box);
-    box_list.put("boxes", box_array);
+    box.put("content", jsonBookArray);
+    boxArray.put(box);
+    boxList.put("boxes", boxArray);
+    return boxList;
   }
   
+  /**
+   * Main method uses path in my local memory to access the HTML files
+   */
   public static void main(String[] args){
     String path = "C:/Users/Roger/Documents/Datafiniti/DataFizz-master/AllMyBooksArePacked/data/book"; //local path
     try{
-      array_constructor(path);
-      json_creator();
-      System.out.println(box_list);
+      JSONObject boxList = buildDataExport(buildBookArray(path));
+      System.out.println(boxList);
     }
     catch(IOException e){}
     catch(JSONException e){}
   }
 }
-
