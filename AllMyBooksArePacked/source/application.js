c9e8b7f98aca7ee33fa80afc5af2496a882046fc
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-07-03 11:11:04 -0400

    Create webscraper object which is created by application.js

--- AllMyBooksArePacked/source/application.js
+++ AllMyBooksArePacked/source/application.js
@@ -1,2 +1,5 @@
 $(document).ready(function(){
+  webscraper = new Webscraper("../data/book1.html")
 })
+
+// create a new webscraper object for each file inside of data
---
+++ AllMyBooksArePacked/source/webscraper.js
@@ -0,0 +1,7 @@
+function Webscraper(url){
+  this.url = url;
+}
+
+
+// needs to scrape certain attributes
+// controllers job to create the book objects with the information webscraper returns
