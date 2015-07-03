0fb8ff6e521c572d876e9a596c481be0b16e6906
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-07-03 11:16:06 -0400

    Create empty scrape instance method for webscraper class

--- AllMyBooksArePacked/source/webscraper.js
+++ AllMyBooksArePacked/source/webscraper.js
@@ -2,6 +2,10 @@ function Webscraper(url){
   this.url = url;
 }
 
+Webscraper.prototype.scrape = function(){
+  console.log("hello")
+}
+
 
 // needs to scrape certain attributes
 // controllers job to create the book objects with the information webscraper returns
