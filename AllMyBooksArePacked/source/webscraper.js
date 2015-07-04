6e93516fd846795bf6354cbae477ca0d0250f95a
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-07-04 16:02:09 -0400

    Clean the price data

--- AllMyBooksArePacked/source/webscraper.js
+++ AllMyBooksArePacked/source/webscraper.js
@@ -9,13 +9,15 @@ Webscraper.prototype.scrapeAndCreateBook = function(warehouse){
       dataType: 'html'
     }).done(function(response){
       var price = $(response).find(".bb_price").text();
+      price = Number(price.slice(2).trim());
       var title = $(response).find("#btAsinTitle").text();
       var author = $(response).find(".byLinePipe").prev("a").text();
       var shippingWeight = $(response).find("li").filter(":contains(Shipping Weight)").text();
       var isbn10 = $(response).find("li").filter(":contains(ISBN-10)").text();
-      warehouse.contents.push(new Book(price, title, author, shippingWeight, isbn10));
+      warehouse.contents.push(new Book(title, author, price, shippingWeight, isbn10));
     })
 }
+
 
 
 // needs to scrape certain attributes
