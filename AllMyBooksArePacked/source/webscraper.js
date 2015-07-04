e3d73525066768173c84e1a178085d3bebcab925
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-07-04 13:53:33 -0400

    Correctly target Shipping Weight and isbn-10 from book-html

--- AllMyBooksArePacked/source/webscraper.js
+++ AllMyBooksArePacked/source/webscraper.js
@@ -11,6 +11,8 @@ Webscraper.prototype.scrape = function(){
       var price = $(response).find(".bb_price").text();
       var title = $(response).find("#btAsinTitle").text();
       var author = $(response).find(".byLinePipe").prev("a").text();
+      var shippingWeight = $(response).find("li").filter(":contains(Shipping Weight)").text();
+      var isbn10 = $(response).find("li").filter(":contains(ISBN-10)").text();
     })
 }
 
