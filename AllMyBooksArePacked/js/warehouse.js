728b9cca954e714d8218c42b5e2522f5d2672ecc
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-07-04 19:51:04 -0400

    Create sortContentsByShippingWeight which does comparison sort 1 time

--- AllMyBooksArePacked/js/warehouse.js
+++ AllMyBooksArePacked/js/warehouse.js
@@ -3,5 +3,30 @@ function Warehouse(){
 }
 
 Warehouse.prototype.packBoxes = function(){
-  console.log(this.contents.length);
+  this.sortContentsByShippingWeight();
+  console.log(this.contents);
 }
+
+Warehouse.prototype.sortContentsByShippingWeight = function(){
+  for (var i = 0; i < this.contents.length - 1; i++){
+    if (this.contents[i].shippingWeight > this.contents[i+1].shippingWeight){
+      var temp = this.contents[i];
+      this.contents[i] = this.contents[i+1];
+      this.contents[i+1] = temp;
+    }
+  }
+}
+
+// do this until the box contents is empty
+// sort the books by shippingWeight
+// if you pass in an empty box..
+// add together the biggest and smallest books weights
+//  if that's over ten
+//    remove the smaller weighted book
+//    call that box done
+// if that's under ten
+//  pass that box back into the cycle
+// if you pass in an already full box..
+//  only add the smallest box
+//  if it's over ten, remove the smallest box and call the box done
+//  if it's under ten, put it through the cycle again
