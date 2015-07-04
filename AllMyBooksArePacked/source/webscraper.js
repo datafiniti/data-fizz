e39770ed4d387cd69cd906a79019f53735ec5e18
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-07-04 16:07:18 -0400

    Clean title data

--- AllMyBooksArePacked/source/webscraper.js
+++ AllMyBooksArePacked/source/webscraper.js
@@ -2,15 +2,28 @@ function Webscraper(url){
   this.url = url;
 }
 
+
+
 Webscraper.prototype.scrapeAndCreateBook = function(warehouse){
   $.ajax({
       url: this.url,
       type: 'get',
       dataType: 'html'
     }).done(function(response){
+
       var price = $(response).find(".bb_price").text();
       price = Number(price.slice(2).trim());
+
       var title = $(response).find("#btAsinTitle").text();
+      var endOfSlice;
+      for (var i = 0; i < title.length; i++){
+        if (title[i]==="["){
+          endOfSlice = i;
+        }
+      }
+      title = title.slice(0, endOfSlice).trim();
+      
+      console.log(title);
       var author = $(response).find(".byLinePipe").prev("a").text();
       var shippingWeight = $(response).find("li").filter(":contains(Shipping Weight)").text();
       var isbn10 = $(response).find("li").filter(":contains(ISBN-10)").text();
