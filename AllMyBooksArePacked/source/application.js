a8d8f2b42afec5c1ce107f1f03e199045185532a
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-07-04 12:55:52 -0400

    In hardcoded book example, change book url to inside of data folder

--- AllMyBooksArePacked/source/application.js
+++ AllMyBooksArePacked/source/application.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
-  webscraper = new Webscraper("book1.html");
+  webscraper = new Webscraper("../data/book1.html");
   webscraper.scrape();
-
 })
 
 // create a new webscraper object for each file inside of data
