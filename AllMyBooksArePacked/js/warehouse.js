4573b07c100646f3a4bf1b482bc26c41c0e01dc3
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-07-04 22:46:08 -0400

    Remove old comments

--- AllMyBooksArePacked/js/warehouse.js
+++ AllMyBooksArePacked/js/warehouse.js
@@ -48,17 +48,3 @@ Warehouse.prototype.sortContentsByShippingWeight = function(){
     }
   }
 }
-
-// do this until the box contents is empty
-// sort the books by shippingWeight
-// if you pass in an empty box..
-// add together the biggest and smallest books weights
-//  if that's over ten
-//    remove the smaller weighted book
-//    call that box done
-// if that's under ten
-//  pass that box back into the cycle
-// if you pass in an already full box..
-//  only add the smallest box
-//  if it's over ten, remove the smallest box and call the box done
-//  if it's under ten, put it through the cycle again
