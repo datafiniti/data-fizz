472a959a5ea34be01b417447424610d15a4a4b50
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-07-05 13:16:22 -0400

    Move scrape and create books method code to application.js

--- AllMyBooksArePacked/js/application.js
+++ AllMyBooksArePacked/js/application.js
@@ -1,9 +1,36 @@
 $(document).ready(function(){
   var warehouse = new Warehouse();
+  var webscraper = new Webscraper();
   for (var i = 1; i < 21; i++){
     var fileName = "../data/book" + i + ".html";
-    webscraper = new Webscraper(fileName);
-    webscraper.scrapeAndCreateBook(warehouse);
+    $.ajax({
+      url: fileName,
+      type: 'get',
+      dataType: 'html'
+    }).done(function(response){
+
+      var price = $(response).find(".bb_price").text();
+      price = Number(price.slice(2).trim());
+
+      var title = $(response).find("#btAsinTitle").text();
+      var endOfSlice;
+      for (var i = 0; i < title.length; i++){
+        if (title[i]==="["){
+          endOfSlice = i;
+        }
+      }
+      title = title.slice(0, endOfSlice).trim();
+
+      var author = $(response).find(".byLinePipe").prev("a").text();
+
+      var shippingWeight = $(response).find("li").filter(":contains(Shipping Weight)").text();
+      shippingWeight = Number(shippingWeight.slice(17, -42));
+
+      var isbn10 = $(response).find("li").filter(":contains(ISBN-10)").text();
+      isbn10 = isbn10.slice(9);
+
+      warehouse.contents.push(new Book(title, author, price, shippingWeight, isbn10));
+    })
   }
   $(document).ajaxStop(function(){
     warehouse.packBoxes();
--- AllMyBooksArePacked/js/webscraper.js
+++ AllMyBooksArePacked/js/webscraper.js
@@ -1,36 +1,8 @@
-function Webscraper(url){
-  this.url = url;
+function Webscraper(){
 }
 
 
 
 Webscraper.prototype.scrapeAndCreateBook = function(warehouse){
-  $.ajax({
-      url: this.url,
-      type: 'get',
-      dataType: 'html'
-    }).done(function(response){
-
-      var price = $(response).find(".bb_price").text();
-      price = Number(price.slice(2).trim());
-
-      var title = $(response).find("#btAsinTitle").text();
-      var endOfSlice;
-      for (var i = 0; i < title.length; i++){
-        if (title[i]==="["){
-          endOfSlice = i;
-        }
-      }
-      title = title.slice(0, endOfSlice).trim();
-
-      var author = $(response).find(".byLinePipe").prev("a").text();
-
-      var shippingWeight = $(response).find("li").filter(":contains(Shipping Weight)").text();
-      shippingWeight = Number(shippingWeight.slice(17, -42));
-
-      var isbn10 = $(response).find("li").filter(":contains(ISBN-10)").text();
-      isbn10 = isbn10.slice(9);
-
-      warehouse.contents.push(new Book(title, author, price, shippingWeight, isbn10));
-    })
+  
 }
