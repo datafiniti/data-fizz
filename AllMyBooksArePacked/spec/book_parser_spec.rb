249f399b5bc9ff0c3b0bcf8cc195a3f65cae7841
Fraser <frasermince@gmail.com>
2015-08-02 11:52:40 -0500

    Add describe to test.

--- AllMyBooksArePacked/spec/book_parser_spec.rb
+++ AllMyBooksArePacked/spec/book_parser_spec.rb
@@ -1,14 +1,16 @@
 require 'spec_helper'
 
 RSpec.describe BookParser do
-  it 'should get a nokogiri document and apply the RuleSet' do
-    book_parser = BookParser.new(['data/book1.html', 'data/book2.html'])
-    rules = double('rules')
-    allow(RuleSet).to receive(:new).and_return(rules)
-    allow(rules)
-      .to receive(:apply_rule_set)
-      .and_return(expected_result[0], expected_result[1])
-    expect(book_parser.parse).to eq(expected_result)
+  describe '#parse' do
+    it 'should get a nokogiri document and apply the RuleSet' do
+      book_parser = BookParser.new(['data/book1.html', 'data/book2.html'])
+      rules = double('rules')
+      allow(RuleSet).to receive(:new).and_return(rules)
+      allow(rules)
+        .to receive(:apply_rule_set)
+        .and_return(expected_result[0], expected_result[1])
+      expect(book_parser.parse).to eq(expected_result)
+    end
   end
 
   def expected_result
