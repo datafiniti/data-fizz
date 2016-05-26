cfe397f7f39aeaa6b270d2efda13c80836c54399
njang <neo.w.jang@gmail.com>
2016-05-26 08:00:00 -0500

    Update Amazon scraper to retrieve book dimensions and shipping weight

--- scrape-amazon.js
+++ scrape-amazon.js
@@ -2,24 +2,34 @@ let axios = require('axios');
 let cheerio = require('cheerio');
 let fs = require('fs'); 
 
-const uri = "https://www.amazon.com/dp/1501180983"
+// const uri = "https://www.amazon.com/dp/1501180983"
+const uri = "https://www.amazon.com/dp/1628600160"
 
 axios.get(uri)
   .then((response) => {
     if(response.status === 200) {
         let $ = cheerio.load(response.data); 
         let name = $('#productTitle').text();
-        let desc = $('#iframeContent').text();
+        // let desc = $('#iframeContent');
+        // console.log(Object.getOwnPropertyNames(desc));
+        // console.log(desc.length)
+
+
+
+        
         let price = $('#buybox').find("span.a-text-strike").text();
-        let dimensions;
+        let productDetails =  $('#productDetailsTable').find('li');       
+        let dimensions = productDetails[5].children[1].data.split('\n')[1].replace(/^[ \t]+/g,'');
+        let weight = productDetails[6].children[1].data.replace(/^[ \t(]+|[()]/g,'');
+        // console.log(dimensions[5].children[0].data)
+        // console.log(dimensions[6].children[0].data)
+
         let imageURLs = [];
         imageURLs.push(Object.keys(JSON.parse($("#imgBlkFront").attr("data-a-dynamic-image"))));
-        // let imageURLs = $("#imgBlkFront").getAttribute("src");
-        let weight;
 
         console.log(`Name: ${name}`);
         console.log(`List Price: ${price}`)
-        console.log(`Description: ${desc}`);
+        // console.log(`Description: ${desc}`);
         console.log(`Product Dimensions: ${dimensions}`)
         console.log(`Image URLs: ${imageURLs}`)
         console.log(`Weight: ${weight}`)
