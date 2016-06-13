02b373c5f9db97b5c7473337ea7a919d9f775919
John Jacob <jacobjohnrobert@gmail.com>
2016-06-13 18:25:17 -0500

    include units in weight regex

--- AllMyBooksArePacked/amzn_regex.js
+++ AllMyBooksArePacked/amzn_regex.js
@@ -13,7 +13,7 @@ module.exports = {
     title : /<span[^>]*id="btAsinTitle"[^>]*>([^<]+)\s*</,
     author : /(?:>([^<]+)<[^>]+>\s*<[^>]+>\(Author\)<|>\s*([a-zA-Z\s]+)<\/a>\s*<span\s*class="byLinePipe"\s*>\s*\(Editor\)\s*<\/span>)/,
     price : /(?:id="actualPriceValue"><b\s*class="priceLarge"[^>]*>\s*\$([0-9]+\.?[0-9]{1,2})\s*<\/b>|Buy New(?:.|\n)*\$([0-9]+\.?[0-9]{1,2}))/,
-    weight : /<b>\s*Shipping Weight:\s*<\/b>\s*([0-9.]+)\s*[a-zA-Z]+\s*/,
+    weight : /<b>\s*Shipping Weight:\s*<\/b>\s*([0-9.]+\s*[a-zA-Z]+)\s*/,
     ISBN10 : /<b>ISBN-10:<\/b>\s*([a-zA-Z0-9]+)/,
   }
 
