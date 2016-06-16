22147557ce2e3dfb594361bc550edc87ff45154f
Rico Rojas <djizco@yahoo.com>
2016-06-15 20:08:40 -0500

    Parse title and author

--- AllMyBooksArePacked/app/index.js
+++ AllMyBooksArePacked/app/index.js
@@ -2,33 +2,41 @@ const cheerio = require('cheerio');
 const fs      = require('fs');
 
 let bookList = createList();
-let bookData = {};
+let bookData = [];
 
 bookList.forEach(function(book){
   fs.readFile(`../data/${book}`, 'utf8', (err, html) => {
+    console.log("-book:", book);
     if (err) { console.error(err); }
     const $ = cheerio.load(html);
 
     // Allows $ to ignore text within nested selectors
-    $.fn.ignore = function(sel){
-      return this.clone().find(sel||">*").remove().end();
+    $.fn.ignore = function(selector){
+      return this.clone().find(selector||">*").remove().end();
     };
 
     let title, author, price, shipping_weight, isbn10;
     let json = {title: '', author: '', price: '', shipping_weight: '', 'isbn-10': ''};
 
+    // parse title, author
     $('#btAsinTitle').filter(function(){
       let data = $(this);
-      // console.log("data:", data.text());
       title = data.ignore('span').html();
-      console.log("title:", title);
+      author = '';
 
+      data.closest('.buying').find('span').find('a').each(function(i, el){
+        if (author) { author += ', ' + el.children[0].data; }
+        else { author = el.children[0].data; }
+      });
+
+      json.title = title;
+      json.author = author;
     });
-    // json.author = $('.buying').find('span').find('a').html();
-    // json.price = $('#actualPriceValue').text() + ' USD';
-    // json.shipping_weight = $('.content').find('ul').find('li:nth-child(7)').text();
 
-    // console.log('json:', json);
+
+
+    console.log('json:', json);
+    bookData.push(json);
   });
 });
 
@@ -39,3 +47,5 @@ function createList(){
   }
   return list;
 }
+
+
