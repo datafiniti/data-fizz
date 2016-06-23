27870e845d4436c8e89e2b72d4da0a9280a92a02
eazy-g <gbrustkern@gmail.com>
2016-06-23 15:34:26 -0500

    more clean up

--- AllMyBooksArePacked/client/app.js
+++ AllMyBooksArePacked/client/app.js
@@ -50,12 +50,15 @@ var App = (function App() {
 
     //setup the box objects
     for (var i = 1; i <= numBoxes; i++) {
-      packingBoxes['box' + i] = ({"id": i, "totalWeight": 0, "contents": []});
+      packingBoxes['box' + i] = ({'id': i, 'totalWeight': 0, 'contents': []});
     }
 
     /**
     * Step 1, sort books by decreasing weight
     * Also note, the Number() with .substr business is because the weight is still in form '1.1 pounds' (string)
+    * I thought about making a for loop to change every value, but figured this would take more time,
+    * particularly with 2 mil books, rather than just doing it inline.
+    * Of course, this would be a non issue if I hadn't made the books display ready before working with them here
     */
     books.sort(function(a, b) {
       return Number(b.shipping_weight.substr(0, 3)) - Number(a.shipping_weight.substr(0, 3));
@@ -66,20 +69,19 @@ var App = (function App() {
     * room leftover out of all the boxes
     */
     books.forEach(function(book) {
-      var boxToPlaceIn = {'weight': 0, 'boxID': 0}, //holder variable
-          canFit = false; //boolean to determine if the current book will fit in any box
+      var boxToPlaceIn = {'weight': 0, 'boxID': 0}; //holder variable to determine which box is best fit
+      var canFit = false; //boolean to determine if the current book will fit in any box
 
       //find out which box to place the current book in
       for (var x in packingBoxes) {
 
         //add the current book's weight to each of the boxes total weight
-        var potentialWeight = packingBoxes[x]['totalWeight'] + Number(book.shipping_weight.substr(0, 3));
+        var potentialWeight = packingBoxes[x].totalWeight + Number(book.shipping_weight.substr(0, 3));
 
-        if(potentialWeight <= 10 && potentialWeight > boxToPlaceIn['weight']){
+        if(potentialWeight <= 10 && potentialWeight > boxToPlaceIn.weight){
           boxToPlaceIn = {'weight': potentialWeight, 'boxID': packingBoxes[x]['id']};
           canFit = true;
         }
-
       }
 
       if (!canFit) {
@@ -87,8 +89,8 @@ var App = (function App() {
         //set boolean which will display error message if the current book won't fix in any of the boxes
         notEnoughBoxes = true;
       } else {
-        packingBoxes['box' + boxToPlaceIn['boxID']]['contents'].push(book);
-        packingBoxes['box' + boxToPlaceIn['boxID']]['totalWeight'] += Number(book.shipping_weight.substr(0, 3));
+        packingBoxes['box' + boxToPlaceIn['boxID']].contents.push(book);
+        packingBoxes['box' + boxToPlaceIn['boxID']].totalWeight += Number(book.shipping_weight.substr(0, 3));
       }
 
     });
@@ -99,7 +101,7 @@ var App = (function App() {
 
     //change total weights back to strings
     for (var z in packingBoxes) {
-      packingBoxes[z]['totalWeight'] = packingBoxes[z]['totalWeight'].toFixed(1).toString() + ' pounds';
+      packingBoxes[z].totalWeight = packingBoxes[z].totalWeight.toFixed(1).toString() + ' pounds';
     }
 
     $boxesDiv.append('<pre>' + JSON.stringify(packingBoxes, null, 4) + '</pre>');
