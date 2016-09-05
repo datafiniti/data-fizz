39009bfad5bdbdfe91c352ae4010ea5ae34d5a67
frankievx <frankievx@gmail.com>
2016-09-05 13:29:34 -0700

    implement body-parser and webpack middleware, and index route

--- Identity/server/express-server.js
+++ Identity/server/express-server.js
@@ -4,3 +4,19 @@ var webpack = require('webpack');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
 var mongoose = require('mongoose');
+
+// Start DB connection
+mongoose.connect(serverConfig.database);
+
+//Body Parser and Webpack Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(webpackDevMiddleware(compiler, {
+	publicPath: '/dist'
+}));
+app.use(webpackHotMiddleware(compiler));
+
+//Index Route
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
