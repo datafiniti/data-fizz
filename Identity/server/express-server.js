5419fe1e96cebb1c30ce8f92e326a9ecc3abced9
frankievx <frankievx@gmail.com>
2016-09-05 13:12:11 -0700

    add express-server

---
+++ Identity/server/express-server.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
+var mongoose = require('mongoose');
