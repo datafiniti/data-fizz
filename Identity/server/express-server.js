54deec769fe0b1a20f26d1eaf14f142bde495ab8
frankievx <frankievx@gmail.com>
2016-09-05 13:48:29 -0700

    implement Hello, world

--- Identity/server/express-server.js
+++ Identity/server/express-server.js
@@ -1,9 +1,16 @@
+var path = require('path')
 var express = require('express');
 var bodyParser = require('body-parser');
 var webpack = require('webpack');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
 var mongoose = require('mongoose');
+
+//Config
+var app = express();
+var serverConfig = require('./server-config.js');
+var webpackConfig = require('../webpack.config.js');
+var compiler = webpack(webpackConfig);
 
 // Start DB connection
 mongoose.connect(serverConfig.database);
@@ -20,3 +27,6 @@ app.use(webpackHotMiddleware(compiler));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
+
+//Set Express to start listening to requests
+app.listen(3000, () => console.log('listening on 3000'));
