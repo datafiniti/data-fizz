0f9d997944ab0a8fe056b3ebde63c09e32b74535
frankievx <frankievx@gmail.com>
2016-09-06 15:52:11 -0700

    implement login, and verify routes

--- Identity/server/models/authModel.js
+++ Identity/server/models/authModel.js
@@ -1,8 +1,19 @@
 var jwt = require('jsonwebtoken');
+var bcrypt = require('bcrypt');
 var User = require('../schemas/User.js');
 var serverConfig = require('../server-config.js')
 
 var saltRounds = 10;
+
+function comparePassword(attemptedPassword, hashedPassword) {
+  return new Promise(function(resolve, reject){
+    bcrypt.compare(attemptedPassword, hashedPassword, function(err, res) {
+      console.log('comparePassword', res);
+      if(err) reject(err)
+      else resolve(res)
+    });
+  });
+}
 
 function login(req, res) {
   User.findOne({
@@ -14,13 +25,16 @@ function login(req, res) {
       res.json({ success: false, message: 'No User with that email' });
     } 
     else if (user) {
-      if (user.password != req.body.password) {
-        res.json({ success: false, message: 'Incorrect Password.' });
-      } 
-      else {
-        var token = jwt.sign(user, serverConfig.secret, { expiresIn: '1 day' });
-        res.json({ success: true, token: token });
-      }   
+      comparePassword(req.body.password, user.password)
+      .then(function(bool){
+        if (!bool) {
+          res.json({ success: false, message: 'Incorrect Password.' });
+        } 
+        else {
+          var token = jwt.sign(user, serverConfig.secret, { expiresIn: '1 day' });
+          res.json({ success: true, token: token });
+        }   
+      });
     }
   });
 }
@@ -28,7 +42,7 @@ function login(req, res) {
 function verify(req, res, next) {
   var token = req.body.token || req.query.token || req.headers['x-access-token'];
   if (token) {
-    jwt.verify(token, app.get('secret'), function(err, decoded) {      
+    jwt.verify(token, serverConfig.secret, function(err, decoded) {      
       if (err) {
         return res.json({ success: false, message: 'Failed to authenticate token.' });    
       } 
--- Identity/server/routes/authRoutes.js
+++ Identity/server/routes/authRoutes.js
@@ -4,11 +4,11 @@ var authRoutes = express.Router();
 
 var authenticate = require('../models/authModel.js');
 
+authRoutes.post('/login', authenticate.login);
+authRoutes.use(authenticate.verify);
 authRoutes.get('/', function(req, res) {
   res.json({ message: 'Testing' });
 });
-authRoutes.post('/login', authenticate.login);
-authRoutes.use(authenticate.verify);
 
 module.exports = authRoutes;
 
