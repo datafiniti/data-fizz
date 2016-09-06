044a3c496ed8ea67959cdc0112a5ea9a26c86cd2
frankievx <frankievx@gmail.com>
2016-09-06 01:43:57 -0700

    implement Authenticate Route

--- Identity/server/routes/authRoutes.js
+++ Identity/server/routes/authRoutes.js
@@ -8,3 +8,26 @@ var app = express();
 var authRoutes = express.Router();
 
 app.set('secret', serverConfig.secret);
+
+var authenticate = function(req, res) {
+  User.findOne({
+    email: req.body.email
+  }, function(err, user) {
+  	console.log(user);
+    if (err) throw err;
+    if (!user) {
+      res.json({ success: false, message: 'No User with that email' });
+    } 
+    else if (user) {
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Incorrect Password.' });
+      } 
+      else {
+        var token = jwt.sign(user, app.get('secret'), { expiresIn: '1 day' });
+        res.json({ success: true, token: token });
+      }   
+    }
+  });
+}
+
+authRoutes.post('/authenticate', authenticate);
