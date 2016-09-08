670d6c44c99150b395aba04c6d400762a5123799
frankievx <frankievx@gmail.com>
2016-09-07 23:01:05 -0700

    add change email form

--- Identity/src/Dashboard/change-email.js
+++ Identity/src/Dashboard/change-email.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
 import styles from './dashboard.css';
 import Paper from 'material-ui/Paper'
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+
 
 //Dashboard Component
 export default class ChangeEmail extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			emailForm: false
+		}
 	};
 
+	handleOpen() {
+		this.setState({ emailForm: true});
+	}
+	handleClose() {
+		this.setState({ emailForm: false});
+	}
+
   render() {
+  	const { emailForm } = this.state;
     return (
-    	<Paper zDepth={2} rounded={true} className={styles.change_email}>
+    	<Paper zDepth={2} rounded={true} className={styles.change_email} onClick={this.handleOpen.bind(this)}>
 	    	<div id='change email' >
 	    		<h1 className={styles.header}> Change Your Email </h1>
 	    	</div>
+	    	<Dialog
+	    		title="Change Email Form"
+	    		open={emailForm}
+	    		onRequestClose={this.handleClose.bind(this)}
+    		/>
     	</Paper>
     );
   }
