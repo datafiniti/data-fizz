2ca525b6bd9ccbaf31027ddf1208b12d5dd8b6b4
Sonam Dolker Kindy <sonam.d.kindy@gmail.com>
2016-10-20 13:14:58 -0700

    Updated initial state for auth reducer, cleaned up formatting and added fetchUser switch case

--- Identity/src/reducers/authentication.js
+++ Identity/src/reducers/authentication.js
@@ -3,31 +3,35 @@ import {
   DEAUTH_USER,
   AUTH_ERROR,
   CLEAR_AUTH_ERROR,
-  FETCH_DATA,
+  FETCH_USER,
 } from '../actions/types';
 
-export default function(state = {}, action) {
+const initState = {
+  userId: null,
+  user: {},
+}
+
+export default function(state = initState, action) {
   switch (action.type) {
     case AUTH_USER:
       return {
         ...state,
-        authenticated: true,
-        error: null,
         userId: action.payload._id,
+        authenticated: true,
       };
     case DEAUTH_USER:
       return {
         ...state,
-        error: null,
-        authenticated: false,
         userId: null,
+        user: {},
+        authenticated: false,
       };
     case AUTH_ERROR:
       return { ...state, error: action.payload };
     case CLEAR_AUTH_ERROR:
       return { ...state, error: null };
-    case FETCH_DATA:
-      return { ...state, message: action.payload };
+    case FETCH_USER:
+      return { ...state, user: action.payload };
   }
   return state;
 }
