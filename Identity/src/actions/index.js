8c26a91062db173effe65661afeb187c36cd164f
Sonam Dolker Kindy <sonam.d.kindy@gmail.com>
2016-10-21 12:26:50 -0700

    Linting and added REQUEST_PASSWORD_RESET action creator

--- Identity/src/actions/index.js
+++ Identity/src/actions/index.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 import {
-  SIGN_UP,
-  SIGN_IN,
   AUTH_USER,
   DEAUTH_USER,
   AUTH_ERROR,
   CLEAR_AUTH_ERROR,
   FETCH_USER,
   UPDATE_USER,
+  REQUEST_PASSWORD_RESET,
 } from './types';
 import { browserHistory } from 'react-router';
 
 const ROOT_URL = 'http://localhost:3080';
 
+/* eslint-disable func-names */
 export function signupUser(user) {
   return function (dispatch) {
     // Making use of redux thunk to return a function (allowing access to dispatch)
@@ -28,12 +28,12 @@ export function signupUser(user) {
         // - Redirect to the protected route /feature
         browserHistory.push('/feature');
       })
-      .catch(({response}) => {
+      .catch(({ response }) => {
         // If request is bad...
         // - Show an error to the user
         dispatch(authError(response.data.error));
       });
-  }
+  };
 }
 
 export function signinUser({ email, password }) {
@@ -51,40 +51,37 @@ export function signinUser({ email, password }) {
         // - Redirect to the protected route /feature
         browserHistory.push('/feature');
       })
-      .catch(({response}) => {
+      .catch(({ response }) => {
         // If request is bad...
         // - Show an error to the user
         dispatch(authError(response.data.error));
       });
-  }
+  };
 }
 
 export function signoutUser(userId) {
-  // localStorage.removeItem('token'); // remove JWT from local storage
-  // return {
-  //   type: DEAUTH_USER
-  // };
   return function (dispatch) {
     // Making use of redux thunk to return a function (allowing access to dispatch)
     // i.e., allowing for asynchronous decision making/requests and then dispatching an action
     // Submit email/password to server
     return axios.post(`${ROOT_URL}/signout`, { userId })
-      .then(response => {
+      .then(() => {
         // If request is good...
         // - Update state to indicate user is de-authenticated
-        dispatch(deauthUser(localStorage.getItem('token'))); // get JWT and call action dispatch deauthUser action creator
+        // get JWT and call action dispatch deauthUser action creator
+        dispatch(deauthUser(localStorage.getItem('token')));
         localStorage.removeItem('token'); // remove JWT from local storage
       })
-      .catch(({response}) => {
+      .catch(({ response }) => {
         // If request is bad...
         dispatch(authError(response.data.error));
       });
-  }
+  };
 }
 
 export function authUser(token) {
-  function getPayloadFrom(token) {
-    return JSON.parse(window.atob(token.match(/([^.]+)/g)[1]));
+  function getPayloadFrom(foundToken) {
+    return JSON.parse(window.atob(foundToken.match(/([^.]+)/g)[1]));
   }
   return {
     type: AUTH_USER,
@@ -114,9 +111,9 @@ export function clearAuthError() {
 }
 
 export function fetchUser() {
-  return function(dispatch) {
+  return function (dispatch) {
     axios.get(`${ROOT_URL}/account`, {
-      headers: { authorization: localStorage.getItem('token') }
+      headers: { authorization: localStorage.getItem('token') },
     })
       .then(response => {
         const user = response.data;
@@ -134,20 +131,20 @@ export function fetchUser() {
           );
         }
       })
-      .catch(({response}) => {
+      .catch(({ response }) => {
         // If authorization header not included for some weird reason...
         dispatch(authError(response.data.error));
       });
-  }
+  };
 }
 
 export function updateUser(user) {
-  return function(dispatch) {
+  return function (dispatch) {
     const params = user;
     const config = {
-        headers: {
-          authorization: localStorage.getItem('token')
-        }
+      headers: {
+        authorization: localStorage.getItem('token'),
+      },
     };
     let url = `${ROOT_URL}/account/edit`;
     if (user.password || user.newPassword) {
@@ -160,15 +157,15 @@ export function updateUser(user) {
           payload: response.data,
         });
       })
-      .catch(({response}) => {
+      .catch(({ response }) => {
         // If authorization header not included for some weird reason...
         dispatch(authError(response.data.error));
       });
-  }
+  };
 }
 
 export function requestPasswordReset(user) {
-  return function(dispatch) {
+  return function (dispatch) {
     const params = { user };
     const url = `${ROOT_URL}/account/request/password/reset`;
     axios.post(url, params)
@@ -178,9 +175,10 @@ export function requestPasswordReset(user) {
           payload: response.data,
         });
       })
-      .catch(({response}) => {
+      .catch(({ response }) => {
         // if there's an error with the backend -- db or nodemailer
         dispatch(authError(response.data.error));
       });
-  }
+  };
 }
+/* eslint-enable func-names */
--- Identity/src/actions/types.js
+++ Identity/src/actions/types.js
@@ -1,4 +1,4 @@
-export const SIGN_IN = 'sign_in'
+export const SIGN_IN = 'sign_in';
 export const AUTH_USER = 'auth_user';
 export const DEAUTH_USER = 'deauth_user';
 export const AUTH_SUCCESS = 'auth_success';
@@ -6,3 +6,4 @@ export const AUTH_ERROR = 'auth_error';
 export const CLEAR_AUTH_ERROR = 'clear_auth_error';
 export const FETCH_USER = 'fetch_user';
 export const UPDATE_USER = 'update_user';
+export const REQUEST_PASSWORD_RESET = 'request_password_reset';
