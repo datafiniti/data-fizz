b7922854343539837883edeb7ee6e17beda5f527
Robbie Zuazua <robbiezuazua@robbies-mbp.attlocal.net>
2016-11-29 22:56:57 -0600

    completed price parsing

--- AllMyBooksArePacked/Parser.py
+++ AllMyBooksArePacked/Parser.py
@@ -12,34 +12,32 @@ from bs4 import BeautifulSoup
 #*********************************************************************
 # Testing: open single document 
 #*********************************************************************
-soup = BeautifulSoup(open("data/book19.html"), "html.parser")
+soup = BeautifulSoup(open("data/book1.html"), "html.parser")
 
 #*********************************************************************
-# Title location: id="btAsinTitle"<..title..>
+# Title location
 #*********************************************************************
 titleContainer = soup.find(id="btAsinTitle")
 title = titleContainer.contents[0]
 print(title);
 
 #*********************************************************************
-# Author location: inside div that contains title and author
+# Author location
 #*********************************************************************
 authorContainer = titleContainer.parent.parent
 author = authorContainer.find('a').text
 print(author)
 
-
 #*********************************************************************
-# ISBN location : id="ASIN" value="..isbn.."
+# ISBN location 
 #*********************************************************************
 isbnContainer = soup.find(id="ASIN")
 isbn = isbnContainer['value']
 print(isbn)
 
-
-#*********************************************************************
-# Shipping weight location : li><b>Shipping Weight:</b> ..weight..
-#*********************************************************************
+#**********************************************************************
+# Shipping weight location 
+#**********************************************************************
 weightSibling = soup.find('b', string='Shipping Weight:')
 weightContainer = weightSibling.parent
 weight = weightContainer.contents[1]
@@ -47,5 +45,15 @@ weight = weightContainer.contents[1]
 #weight always has form " x.x pounds (" we need to clean it up
 weight = weight[1:-2]									
 print (weight)
+
+#*********************************************************************
+# Price location 
+#*********************************************************************
+priceContainer = soup.find(id="actualPriceContent")
+price = priceContainer.contents[0].text
+print(price)
+
+
+
 
 
