2ecbfca20a1b02d897d9f89ea77303fd95a0fe7a
Brandon Flint <flint622@gmail.com>
2016-12-20 13:28:43 -0500

    Finished scrapping isbn10, author, weight. json files contains all appropriate data.

--- AllMyBooksArePacked/scraper.js
+++ AllMyBooksArePacked/scraper.js
@@ -25,22 +25,30 @@ function scrapeBook(html) {
 		json.title = title;
 	})
 	$('#handleBuy div.buying span a').each(function(i, element) {
-		var el = $(this);
-		var author = el.text();
-		json.author = author;
-		console.log(author);
+		if (i == 0) {
+			var el = $(this);
+			var author = el.text();
+			json.author = author;
+		}
 	})
-
 	$('span#actualPriceValue').each(function(i, element) {
 		var el = $(this);
 		var price = el.text();
 		json.price = price;
 	})
-	$('table#productDetailsTable li').contents().each(function(i, element) {
+	$('table#productDetailsTable ul li').each(function(i, element) {
 		var el = $(this);
-		var shippingWeight = el.text();
-		json.shippingWeight = shippingWeight;
-		//console.log($(this))
+		var text = el.text();
+		if (text.includes('Shipping Weight: ')){
+			var start = text.indexOf(':') + 2;
+			var end = text.indexOf('View', start) - 2;
+			text = text.substring(start, end);
+			json.shippingWeight = text;			
+		}
+		if (text.includes('ISBN-10: ')){
+			json.isbn10 = text.split(': ').pop();	
+		}		
+
 	})
 
 	fs.writeFile(json.title + '.json', JSON.stringify(json, null, 4), function(err) {
