469dbeb6091b395f85d100c943c7b951c5573971
brandon flint <flint622@gmail.com>
2016-12-21 23:57:28 -0500

    json to sorted array by book weight. Started packaging.

--- AllMyBooksArePacked/boxPacker.js
+++ AllMyBooksArePacked/boxPacker.js
@@ -1,13 +1,55 @@
 var fs = require('fs');
 var dataFolder = './jsonData/';
 
-var obj;
-fs.readdir(dataFolder, (err, files) => {
-	files.forEach(file => {
-		fs.readFile(dataFolder + file, 'utf8', function (err, data) {
-			if (err) throw err;
-			obj = JSON.parse(data);
-			console.log (obj);
-		})
-	})
-})
+
+jsonArr = jsonFilesToSortedArray(dataFolder);
+
+
+//console.log(jsonArr[0]['shippingWeight']);
+packing(jsonArr, 10);
+
+
+
+
+function jsonFilesToSortedArray(dir) {
+    var results = [];
+    fs.readdirSync(dir).forEach(function(file) {
+    	var json = JSON.parse(require('fs').readFileSync(dataFolder + file, 'utf8'));
+    	results.push(json);
+    });
+    results.sort(function(a, b) {
+    	return parseFloat(a.shippingWeight) - parseFloat(b.shippingWeight);
+	}).reverse();
+
+    return results;
+};
+
+function packing(arr, max) {
+	if(arr.length > 0) {
+		var box = {
+			id: "",
+			totalWeight: "",
+			contents: ""
+		}
+
+		var largest = arr.shift();
+		var totalWeight = parseFloat(largest['shippingWeight']);
+		totalWeight += parseFloat(arr[arr.length / 2]['shippingWeight']);
+		if ( totalWeight + parseFloat(arr[arr.length / 2]['shippingWeight']) > max )
+		var contents = largest;
+
+		console.log(totalWeight);
+		//packing(arr, max)
+	}
+}
+
+function test(arr, max, weight) {
+	testArr = [];
+	if( weight == max || arr.length == 0 ) {
+		return testArr
+	}
+	else{
+		testWeight = parseFloat(arr[arr.length/2]['shippingWeight']);
+		if ( (weight + testWeight) > max
+	}
+}
