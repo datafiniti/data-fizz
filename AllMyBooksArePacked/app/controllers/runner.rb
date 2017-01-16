f90a1f8a5797d08087165cf1ca9694e775a0871d
vernistage <sdickers10@gmail.com>
2017-01-16 17:22:46 -0600

    test parsing methods

--- AllMyBooksArePacked/app/controllers/runner.rb
+++ AllMyBooksArePacked/app/controllers/runner.rb
@@ -4,5 +4,15 @@
 # save each bit of information to db
 
 get '/' do
-  Parser.parse_dir('data')
+  # file = Parser.process_file('data/book1.html')
+  # title = Parser.grab_title(file)
+  # author = Parser.grab_author(file)
+  # price = Parser.grab_price(file)
+  # weight = Parser.grab_weight(file)
+  # isbn = Parser.grab_isbn(file)
+  # puts "Title: #{title} \n
+  # Author: #{author} \n
+  # Price: #{price} \n
+  # Weight: #{weight} \n
+  # ISBN: #{isbn}"
 end
