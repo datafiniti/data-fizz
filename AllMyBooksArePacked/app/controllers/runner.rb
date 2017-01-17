c18c196bb761beac7fd2c3e71ddba204dc1c9022
vernistage <sdickers10@gmail.com>
2017-01-17 11:19:02 -0600

    pseudocode next steps

--- AllMyBooksArePacked/app/controllers/runner.rb
+++ AllMyBooksArePacked/app/controllers/runner.rb
@@ -1,7 +1,6 @@
-# Iterate over each document in data
-# for each document, call all parsable methods
-# separate each bit of information
-# save each bit of information to db
+# create Box object with id, totalweight as string, 'has many books'
+# divide into N boxes for shipping with each box having no more than 10lbs of books.
+# Output formatted JSON doc
 
 get '/' do
   if Book.count > 0
