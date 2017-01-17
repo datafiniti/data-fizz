d324fe1b30b48b74265242a1c5335dc07beacb27
vernistage <sdickers10@gmail.com>
2017-01-17 11:26:31 -0600

    organize pseudocode for next steps

--- AllMyBooksArePacked/app/controllers/runner.rb
+++ AllMyBooksArePacked/app/controllers/runner.rb
@@ -1,7 +1,3 @@
-# create Box object with id, totalweight as string, 'has many books'
-# divide into N boxes for shipping with each box having no more than 10lbs of books.
-# Output formatted JSON doc
-
 get '/' do
   if Book.count > 0
     @books = Book.all
--- AllMyBooksArePacked/app/models/box.rb
+++ AllMyBooksArePacked/app/models/box.rb
@@ -2,4 +2,13 @@ class Box < ActiveRecord::Base
   has_many :books
 
   validates :totalWeight, presence: true
+
+  def package
+  # divide into N boxes for shipping with each box having no more than 10lbs of books.
+  end
+
+  def packing_list
+  # Output formatted JSON doc
+  end
+
 end
