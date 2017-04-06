e2c9ddf42ed339bddc2993931065ecdb22e5dc25
Forrest Collins <Forest.d.collins@gmail.com>
2017-04-06 16:51:39 -0500

    Added a small json response helper method

---
+++ Identity/server/helpers/json.js
@@ -0,0 +1,30 @@
+function good(obj, res) {
+	res.send({
+		success: 1,
+		res: obj
+	});
+}
+
+function bad(err, res) {
+	let obj = {
+		success: 0,
+		res: err
+	}
+
+	if (obj.res.errors) {
+		obj.res.messages = [];
+
+		for (let i in obj.res.errors) {
+			obj.res.messages.push(obj.res.errors[i].message);
+		}
+
+		obj.res.message = obj.res.messages[0]''
+	}
+
+	res.send(obj);
+}
+
+export default {
+	good: good,
+	bad: bad
+} 
