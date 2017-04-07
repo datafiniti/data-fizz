aa3f72bb103079a5b03ca48c8e0541ad22c8d607
Forrest Collins <Forest.d.collins@gmail.com>
2017-04-07 17:15:24 -0500

    Index route now loads the auth container when it isn't authed, will split out auth from child state soon and pass as a wrapping function for the routes

--- Identity/app/components/home/home.jsx
+++ Identity/app/components/home/home.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
+import Auth from '../auth/auth';
 
 class Home extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			authenticated: false,
+		};
+	}
+
 	render() {
+		if (!this.state.authenticated) {
+			return (
+				<div>
+					<Auth />
+				</div>
+			);
+		}
+
 		return (
-			<div><p>Authed</p></div>
+			<div><p>Home</p></div>
 		);
 	}
 }
