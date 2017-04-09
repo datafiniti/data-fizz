671a2a104cd625f47dc67a06cb44e7116e360c20
Forrest Collins <Forest.d.collins@gmail.com>
2017-04-08 22:09:31 -0500

    Added rate limiting to the user model

--- Identity/server/models/users.js
+++ Identity/server/models/users.js
@@ -43,10 +43,37 @@ const UserSchema = new mongoose.Schema({
 		default: false
 	},
 
+	loginAttempts: {
+		type: Number,
+		default: 0,
+		required: true
+	},
+
+	lockUntil: {
+		type: Number
+	},
+
+	limitReached: {
+		type: Number,
+		required: true,
+		default: 0
+	},
+
+	secureLock: {
+		type: Boolean,
+		default: false
+	},
+
 	token: String,
 	resetPasswordToken: String,
 	resetPasswordExpires: Date
 });
+
+UserSchema.virtual('isLocked').get(function() {
+	let self = this;
+
+	return !!(self.lockUntil && self.lockUntil > Date.now());
+})
 
 UserSchema.pre('save', function(next) {
 	let user = this;
@@ -84,6 +111,56 @@ UserSchema.methods = {
 
 			cb(null, isMatch);
 		});
+	},
+
+	incorrectLoginAttempts: function(cb) {
+		if (this.lockUntil && this.lockUntil < Date.now()) {
+			return this.update({
+				$set: {
+					loginAttempts: 1,
+					limitReached,
+				},
+
+				$unset: {
+					lockUntil: 1,
+				},
+			}, cb);
+		}
+
+		let updates = {
+			$inc: { loginAttempts: 1 }
+		};
+
+		if (this.loginAttempts + 1 > 5 && !this.isLocked) {
+			updates.$set = {
+				lockUntil: Date.now() + 2 * 60 * 60 * 1000,
+				limitReached: 1,
+			};
+		}
+
+		if (this.loginAttempts + 1 > 5 && this.limitReached === 1 && !this.isLocked) {
+			updates.$set = {
+				lockUntil: Date.now() + 4 * 60 * 60 * 1000,
+				limitReached: 2,
+			};
+		}
+
+		if (this.loginAttempts + 1 > 3 && this.limitReached === 2 && !this.isLocked) {
+			updates.$set = {
+				lockUntil: Date.now() + 8 * 60 * 60 * 1000,
+				limitReached: 3,
+			};
+		}
+
+		if (this.loginAttempts + 1 > 3 && this.limitReached == 3 && !this.isLocked) {
+			updates.$set = {
+				lockUntil: Date.now() + 10000 * 60 * 60 * 1000,
+				limitReached: 4,
+				secureLock: true,
+			};
+		}
+
+		return this.update(updates, cb);
 	},
 
 	toJSON: function() {
