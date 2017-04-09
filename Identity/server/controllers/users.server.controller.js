879548cba0df616761652b4cdcac07b0ba08fad7
Forrest Collins <Forest.d.collins@gmail.com>
2017-04-08 22:19:06 -0500

    Added the rate limiting for the login route

--- Identity/server/controllers/users.server.controller.js
+++ Identity/server/controllers/users.server.controller.js
@@ -40,6 +40,20 @@ module.exports = () => {
 				return json.bad(err, res);
 			}
 
+			if (user.isLocked) {
+				return user.incorrectLoginAttempts((err) => {
+					if (err) {
+						return json.bad(err, res);
+					}
+
+					json.bad({message: `Sorry, your have reached the maximum number of login attempts and your account has been locked until ${user.lockUntil}`}, res);
+				})
+			}
+
+			if (user.secureLock) {
+				return json.bad({message: 'Sorry, you account has been permenantly locked due to excessive attempts at logging into your account. Please contact the staff to sort this out'}, res);
+			}
+
 			if (!user) {
 				return json.bad({message: 'Sorry, there was no user with that email/password combination'}, res);
 			}
@@ -50,18 +64,59 @@ module.exports = () => {
 				}
 
 				if (isMatch) {
-					user.token = generateToken(user);
-					user.save((err) => {
+					if (!user.loginAttempts && !user.lockUntil
+						 && !user.secureLock) {
+						user.token = generateToken(user);
+						user.save((err) => {
+							if (err) {
+								return json.bad(err, res);
+							}
+
+							json.good({
+								record: user,
+								token: user.token
+							}, res)
+						});
+					}
+
+					let updates = {
+						$set: {
+							loginAttempts: 0,
+							limitReached: 0,
+						},
+
+						$unset: { lockUntil: 1 }
+					};
+
+					return user.update(updates, (err, item) => {
+						let token = generateToken(user);
+
 						if (err) {
 							return json.bad(err, res);
 						}
 
 						json.good({
 							record: user,
-							token: user.token
+							token: token,
 						}, res)
 					});
 				}
+
+				user.incorrectLoginAttempts((err) => {
+					let totalAttempts;
+
+					if (err) {
+						return json.bad(err, res);
+					}
+
+					if (user.limitReached >= 2) {
+						totalAttempts = 3;
+					} else {
+						totalAttempts = 5;
+					}
+
+					json.bad({message: `Sorry, either your email or password were incorrect. You have ${totalAttempts - user.loginAttempts} remaining until your account is locked`}, res);
+				});
 			});
 		});
 	};
