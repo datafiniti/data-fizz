de204163236f6e51998b64692ddc98ba200c63e3
Siwady <edward@acklenavenue.com>
2017-12-28 08:47:12 -0600

    adding sort before iterating

--- AllMyBooksArePacked/src/amazon/packer.js
+++ AllMyBooksArePacked/src/amazon/packer.js
@@ -7,13 +7,22 @@ const addBox = (boxes, currentIndex, currentWeight, currentContents) => {
         contents: currentContents
     });
     return boxes;
-}
+};
+const sortBooks = (books) => {
+    const sortedBooks = books.sort(function(book1, book2) {
+        const weight1 = parseFloat(book1.shippingWeight.substring(0, book1.shippingWeight.indexOf('pounds')));
+        const weight2 = parseFloat(book2.shippingWeight.substring(0, book2.shippingWeight.indexOf('pounds')));
+        return weight2 - weight1;
+    });
+    return sortedBooks;
+};
 const packBooksInBoxes = (books) => {
     let boxes = [];
     let currentIndex = 1;
     let currentWeight = 0;
     let currentContents = [];
-    books.forEach((book, index) => {
+    const sortedBooks = sortBooks(books);
+    sortedBooks.forEach((book, index) => {
         const tempWeight = parseFloat(book.shippingWeight.substring(0, book.shippingWeight.indexOf('pounds')));
         if(currentWeight + tempWeight > 10) {
             boxes = addBox(boxes, currentIndex++, currentWeight, currentContents);
