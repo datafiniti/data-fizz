5a68dbb33a43a672dea2928970c9beb1e580338d
Nicholas Wilcox <nwilcox@oberlin.edu>
2018-05-15 14:15:06 -0700

    Set up Maven with JUnit, executable .jar, and main class BookPacker.

---
+++ AllMyBooksArePacked/src/main/java/bookpacker/BookPacker.java
@@ -0,0 +1,13 @@
+package bookpacker;
+
+public class BookPacker {
+
+	public static void main(String[] args) {
+		System.out.println(String.format("Hello, %s", args[0]));
+	}
+
+	public int someMethod() {
+		return 3;
+	}
+
+}
---
+++ AllMyBooksArePacked/src/test/java/bookpacker/BookPackerTest.java
@@ -0,0 +1,17 @@
+package bookpacker;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BookPackerTest {
+
+	private BookPacker bkpkr = new BookPacker();
+
+	@Test
+	public void testSomeMethod() {
+		assertEquals(3, bkpkr.someMethod());;
+	}
+
+
+}
