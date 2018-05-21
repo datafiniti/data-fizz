108423b62641e0730e6906f84a96cf24e908d843
Mark Alaniz <markalaniz000@gmail.com>
2018-05-21 17:57:56 -0500

    initial commit with gitignore this time

--- AllMyBooksArePacked/app.js
+++ AllMyBooksArePacked/app.js
@@ -2,21 +2,7 @@ const fs = require('fs');
 const cheerio = require('cheerio');
 const $ = cheerio.load();
 
-// constuctor for making boxes
-function box(id) {
-    this.id = id
-    this.totalWeight = function() {
-        let weight = 0;
-        this.contents.map(i => {
-            weight += this.contents.shipping_weight
-        });
-        return weight;
-    },
-    this.contents = [];
-}
-
-const mybox = new box(1);
-console.log(mybox.totalWeight());
+// Constuctor for making boxes
 
 // json variable that will be used as the output
 const boxJSON = {}
