082fe1b0311096df93ced32c481772f83f32f92f
Mark Alaniz <markalaniz000@gmail.com>
2018-05-23 15:05:42 -0500

    Added loop to access files from the data directory and cherio can now access data from the html

--- AllMyBooksArePacked/app.js
+++ AllMyBooksArePacked/app.js
@@ -1,15 +1,25 @@
-const fs = require('fs');
-const cheerio = require('cheerio');
-const $ = cheerio.load();
+// Require Dependencies
+const fs = require("fs");
+const cheerio = require("cheerio");
 
 // Constuctor for making boxes
 
 // json variable that will be used as the output
-const boxJSON = {}
+const boxJSON = {};
+
+// Count number of files in directory so we can loop over any amount in the future
+const dir = "./data";
+const files = fs.readdirSync(dir);
+
 
 // Filesystem code that will be doing the scraping
-const readMe = fs.readFileSync('./data/book1.html', 'utf8');
-
+for (let i = 0; i < files.length; i++) {
+  fs.readFile(`./data/${files[i]}`, "utf8", function(err, result) {
+    const $ = cheerio.load(result);
+    const title = $("#btAsinTitle").text();
+    console.log(title);
+  });
+}
 
 // Output file that will be the raw json of the boxes
-fs.writeFileSync('Output.json');
+fs.writeFileSync("Output.json");
