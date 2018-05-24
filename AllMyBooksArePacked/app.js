399e6e4e302e0e76132060e641af199a56a5279d
Mark Alaniz <markalaniz000@gmail.com>
2018-05-24 01:30:42 -0500

    Working basic scraper with a few mismatching fields. Working output json file. Need to work on correcting the mismatching fields.

--- AllMyBooksArePacked/app.js
+++ AllMyBooksArePacked/app.js
@@ -1,25 +1,49 @@
 // Require Dependencies
 const fs = require("fs");
 const cheerio = require("cheerio");
-
-// Constuctor for making boxes
+const Box = require("./constructors/Box.js");
+const Contents = require("./constructors/Contents.js");
 
 // json variable that will be used as the output
-const boxJSON = {};
+const outputJSON = {};
 
 // Count number of files in directory so we can loop over any amount in the future
 const dir = "./data";
 const files = fs.readdirSync(dir);
 
+// set up Initial box for storing books
+const boxCount = 1;
+outputJSON["Box" + boxCount] = new Box(boxCount);
 
 // Filesystem code that will be doing the scraping
+// ${files[i]}
 for (let i = 0; i < files.length; i++) {
   fs.readFile(`./data/${files[i]}`, "utf8", function(err, result) {
     const $ = cheerio.load(result);
-    const title = $("#btAsinTitle").text();
-    console.log(title);
+    const title = $("#btAsinTitle")
+      .text()
+      .trim();
+    const author = $("#handleBuy .buying span a")
+      .first()
+      .text()
+      .trim();
+    const price = $("#actualPriceValue .priceLarge").text() + " USD";
+    const shipping_weight = $(
+      "#productDetailsTable .content ul li:nth-child(7)"
+    )
+      .text()
+      .trim();
+    const isbn10 = $("#productDetailsTable .content ul li:nth-child(4)")
+      .text()
+      .trim();
+    const content = { title, author, price, shipping_weight, isbn10 };
+    console.log(content);
+    outputJSON.Box1.contents.push(content);
+
+    // Output file that will be the raw json of the boxes
+    fs.writeFileSync("outputJSON.json", JSON.stringify(outputJSON, null, 2));
   });
 }
 
 // Output file that will be the raw json of the boxes
-fs.writeFileSync("Output.json");
+// fs.writeFileSync("outputJSON.json", JSON.stringify(outputJSON, null, 2));
