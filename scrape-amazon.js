5b3a3f74ef414fe52a2ec951e82ba6b1c6e7b892
njang <neo.w.jang@gmail.com>
2018-05-25 09:15:00 -0500

    Update Amazon scraper to retrieve book title

--- scrape-amazon.js
+++ scrape-amazon.js
@@ -7,8 +7,19 @@ const uri = "https://www.amazon.com/dp/1501180983"
 axios.get(uri)
   .then((response) => {
     if(response.status === 200) {
-        const html = response.data;
-        const $ = cheerio.load(html); 
-        console.log($('#productTitle').text());
+        let $ = cheerio.load(response.data); 
+        let name = $('#productTitle').text();
+        let desc = $('#iframeContent').text();
+        let price;
+        let dimensions;
+        let imageURLs;
+        let weight;
+
+        console.log(`Name: ${name}`);
+        console.log(`List Price: ${price}`)
+        console.log(`Description: ${desc}`);
+        console.log(`Product Dimensions: ${dimensions}`)
+        console.log(`Image URLs: ${imageURLs}`)
+        console.log(`Weight: ${weight}`)
       }
     }, (error) => console.log(err) );
