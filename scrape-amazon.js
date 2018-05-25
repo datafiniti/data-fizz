4a68039fa601abdee05d29663231f861ad213139
njang <neo.w.jang@gmail.com>
2018-05-25 09:12:00 -0500

    Add axios call to scrape book title

--- scrape-amazon.js
+++ scrape-amazon.js
@@ -1,29 +1,14 @@
-const request = require('request');
-const cheerio = require('cheerio');
-const rp = require('request-promise');
-const fs = require('fs');
+let axios = require('axios');
+let cheerio = require('cheerio');
+let fs = require('fs'); 
 
 const uri = "https://www.amazon.com/dp/1501180983"
 
-request(uri, function(error, response, body) {
-  if(error) {
-    console.log("Error: " + error);
-  }
-  console.log("Status code: " + response.statusCode);
-
-  var $ = cheerio.load('<span id="productTitle">...</span>');
-  // const $ = cheerio.load();
-  console.log($().text())
-
-
-  $('div#siteTable > div.link').each(function( index ) {
-
-    // var title = $(this).find('p.title > a.title').text().trim();
-    // var score = $(this).find('div.score.unvoted').text().trim();
-    // var user = $(this).find('a.author').text().trim();
-    // console.log("Score: " + score);
-    // console.log("User: " + user);
-    // fs.appendFileSync('amazon.txt', title + '\n' + score + '\n' + user + '\n');
-  });
-
-});
+axios.get(uri)
+  .then((response) => {
+    if(response.status === 200) {
+        const html = response.data;
+        const $ = cheerio.load(html); 
+        console.log($('#productTitle').text());
+      }
+    }, (error) => console.log(err) );
