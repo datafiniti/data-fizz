99df77b050bea9c1a13a8d5b70a66042afbc65e5
njang <neo.w.jang@gmail.com>
2018-05-25 08:10:00 -0500

    Update scaper script for Amazon

--- scrape-amazon.js
+++ scrape-amazon.js
@@ -2,22 +2,27 @@ var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
 
-request("https://old.reddit.com", function(error, response, body) {
+var url = "https://www.amazon.com/dp/1501180983"
+
+request(url, function(error, response, body) {
   if(error) {
     console.log("Error: " + error);
   }
   console.log("Status code: " + response.statusCode);
 
-  var $ = cheerio.load(body);
+  var $ = cheerio.load('<span id="productTitle">...</span>');
+  // const $ = cheerio.load();
+  console.log($().text())
+
 
   $('div#siteTable > div.link').each(function( index ) {
-    var title = $(this).find('p.title > a.title').text().trim();
-    var score = $(this).find('div.score.unvoted').text().trim();
-    var user = $(this).find('a.author').text().trim();
-    console.log("Title: " + title);
-    console.log("Score: " + score);
-    console.log("User: " + user);
-    fs.appendFileSync('reddit.txt', title + '\n' + score + '\n' + user + '\n');
+
+    // var title = $(this).find('p.title > a.title').text().trim();
+    // var score = $(this).find('div.score.unvoted').text().trim();
+    // var user = $(this).find('a.author').text().trim();
+    // console.log("Score: " + score);
+    // console.log("User: " + user);
+    // fs.appendFileSync('amazon.txt', title + '\n' + score + '\n' + user + '\n');
   });
 
 });
