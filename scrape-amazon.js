2a2628cd58c4c13540f105ed68b235f3c9587860
njang <neo.w.jang@gmail.com>
2018-05-26 08:15:00 -0500

    Add retrieve logic for book ASIN ID

--- scrape-amazon.js
+++ scrape-amazon.js
@@ -5,20 +5,31 @@ let fs = require('fs');
 // const uri = "https://www.amazon.com/dp/1501180983"
 const uri = "https://www.amazon.com/dp/1628600160"
 
+class Book {
+  constructor(name, price, description, dimensions, imageURLs, weight) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.dimensions = dimensions;
+    this.imageURLs = imageURLs;
+    this.weight = weight;
+  }
+}
+
 axios.get(uri)
   .then((response) => {
     if(response.status === 200) {
         let $ = cheerio.load(response.data); 
         let name = $('#productTitle').text();
+        let desc = "Hello world";
         // let desc = $('#iframeContent');
         // console.log(Object.getOwnPropertyNames(desc));
         // console.log(desc.length)
-
-
-
         
-        let price = $('#buybox').find("span.a-text-strike").text();
+        let price = parseFloat($('#buybox').find("span.a-text-strike").text().replace(/\$/g,''));
         let productDetails =  $('#productDetailsTable').find('li');       
+        let id = productDetails[3].children[1].data.replace(/^[ \t(]+/g,'');
+        
         let dimensions = productDetails[5].children[1].data.split('\n')[1].replace(/^[ \t]+/g,'');
         let weight = productDetails[6].children[1].data.replace(/^[ \t(]+|[()]/g,'');
         // console.log(dimensions[5].children[0].data)
@@ -30,8 +41,10 @@ axios.get(uri)
         console.log(`Name: ${name}`);
         console.log(`List Price: ${price}`)
         // console.log(`Description: ${desc}`);
-        console.log(`Product Dimensions: ${dimensions}`)
-        console.log(`Image URLs: ${imageURLs}`)
-        console.log(`Weight: ${weight}`)
+        console.log(`ASIN: ${id}`)
+        
+        // console.log(`Product Dimensions: ${dimensions}`)
+        // console.log(`Image URLs: ${imageURLs}`)
+        // console.log(`Weight: ${weight}`)
       }
     }, (error) => console.log(err) );
