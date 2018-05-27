f50e85792af1d567d4b7d265c0f0237c9797ed50
Mark Alaniz <markalaniz000@gmail.com>
2018-05-26 19:31:29 -0500

    trying to seperate code for readability and testing reasons

--- AllMyBooksArePacked/app.js
+++ AllMyBooksArePacked/app.js
@@ -1,7 +1,8 @@
 // Require Dependencies
 const fs = require("fs");
-const cheerio = require("cheerio");
+// const cheerio = require("cheerio");
 const Box = require("./constructors/Box.js");
+const scrape = require('./scripts/scrape.js');
 
 // json variable that will be used as the output
 const outputJSON = {};
@@ -21,59 +22,63 @@ for (let i = 0; i < files.length; i++) {
     if (err) {
       console.log(err);
     }
-    const $ = cheerio.load(result);
-    const title = $("#btAsinTitle")
-      .text()
-      .trim();
-    const author = $("#handleBuy .buying span a")
-      .first()
-      .text()
-      .trim();
-    const price = $("#actualPriceValue .priceLarge").text() + " USD";
-    const shipRegex = /(<li><b>Shipping Weight:<\/b>).+/g;
-    const shipping_weight = $("#productDetailsTable .content ul")
-      .html()
-      .trim()
-      .match(shipRegex)[0]
-      .split(" ")
-      .slice(2, 4)
-      .join(" ");
-    const isbn10 = $("#productDetailsTable .content ul li:nth-child(4)")
-      .text()
-      .trim()
-      .split(" ")[1];
-    const content = {
-      title,
-      author,
-      price,
-      shipping_weight,
-      "isbn-10": isbn10
-    };
+    scrape(result);
+    // const $ = cheerio.load(result);
+    // const title = $("#btAsinTitle")
+    //   .text()
+    //   .trim();
+    // const author = $("#handleBuy .buying span a")
+    //   .first()
+    //   .text()
+    //   .trim();
+    // const price = $("#actualPriceValue .priceLarge").text() + " USD";
+    // const shipRegex = /(<li><b>Shipping Weight:<\/b>).+/g;
+    // const shipping_weight = $("#productDetailsTable .content ul")
+    //   .html()
+    //   .trim()
+    //   .match(shipRegex)[0]
+    //   .split(" ")
+    //   .slice(2, 4)
+    //   .join(" ");
+    // const isbnRegex = /(<li><b>ISBN-10:<\/b>).\w+/g;
+    // const isbn10 = $("#productDetailsTable .content ul")
+    //   .html()
+    //   .trim()
+    //   .match(isbnRegex)[0]
+    //   .split(" ")
+    //   .slice(1)
+    //   .join("");
+    // const content = {
+    //   title,
+    //   author,
+    //   price,
+    //   shipping_weight,
+    //   "isbn-10": isbn10
+    // };
 
     // Set up individual variables to see if a new box is needed or not
     const individualBookWeight = parseFloat(
-      content.shipping_weight.split(" ")[0]
+      scrape(result).shipping_weight.split(" ")[0]
     );
-    // console.log(individualBookWeight);
-    // console.log(content.shipping_weight.split(' ')[2]);
     let currentTotalWeight = outputJSON["Box" + boxCount].totalWeight;
 
     // Condition statement for deciding which box this current book will go into.
     currentTotalWeight += individualBookWeight;
     if (currentTotalWeight <= 10) {
       outputJSON["Box" + boxCount].totalWeight += individualBookWeight;
-      outputJSON["Box" + boxCount].contents.push(content);
+      outputJSON["Box" + boxCount].contents.push(scrape(result));
     } else {
       boxCount++;
       outputJSON[""];
       outputJSON["Box" + boxCount] = new Box(boxCount);
       outputJSON["Box" + boxCount].totalWeight += individualBookWeight;
-      outputJSON["Box" + boxCount].contents.push(content);
+      outputJSON["Box" + boxCount].contents.push(scrape(result));
     }
 
     // Output file that will be the raw json of the boxes
     if (i === files.length - 1) {
       fs.writeFileSync("outputJSON.json", JSON.stringify(outputJSON, null, 2));
     }
+    return result;
   });
 }
