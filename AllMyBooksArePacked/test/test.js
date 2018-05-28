4304f973a6975eea10ab0bae810c696d1d79b9d3
Mark Alaniz <markalaniz000@gmail.com>
2018-05-27 23:56:04 -0500

    finishing changes to files and dependencies

--- AllMyBooksArePacked/test/test.js
+++ AllMyBooksArePacked/test/test.js
@@ -1,20 +1,15 @@
 // install testing dependencies
 const expect = require("chai").expect;
-const chai = require('chai');
-chai.use(require('chai-json'));
 const fs = require("fs");
 const createBox = require("../scripts/createBox");
 const myoutput = require('../outputJSON.json');
-const output = JSON.parse(fs.readFileSync("./outputJSON.json", "utf8"));
-
-console.log(myoutput);
 
 describe("Output JSON File", () => {
   it("Should be an object", () => {
-    expect(output).to.be.an("object");
+    expect(myoutput).to.be.an("object");
   });
   it("Weight to be returned as a string", () => {
-    expect(output.Box1.totalWeight).to.be.a("string");
+    expect(myoutput.Box1.totalWeight).to.be.a("string");
   });
 });
 
