bfa1b4dedd1aee9c84949abc01760f9798582b0e
njang <neo.w.jang@gmail.com>
2018-05-28 08:00:00 -0500

    Complete scrape logic for a single book

--- scrape-amazon.js
+++ scrape-amazon.js
@@ -2,8 +2,8 @@ let axios = require('axios');
 let cheerio = require('cheerio');
 let fs = require('fs'); 
 
-const uri = "https://www.amazon.com/dp/1501180983"
-// const uri = "https://www.amazon.com/dp/1628600160"
+// const uri = "https://www.amazon.com/dp/1501180983"
+const uri = "https://www.amazon.com/dp/1628600160"
 
 class Book {
   constructor(id, name, price, description, dimensions, imageURLs, weight) {
@@ -32,15 +32,17 @@ axios.get(uri)
         // console.log($('#bookDesc_iframe_wrapper')[0] == $('#postBodyPS')[0].childNodes[1]);
         // console.log($('#bookDesc_iframe').contentDocument.childNodes[0].childNodes[1].childNodes[1])
         // console.log($('#bookDesc_iframe')[0].contentDocument)
-        let desc = $("meta[name='description']")[0].attribs.content;
-        console.log(desc);
-
         // console.log($('iframe#bookDesc_iframe').contentDocument.body.childNodes[1].innerHTML);
         // console.log($("#bookDesc_iframe_wrapper").length)
         // console.log(Object.getOwnPropertyNames($("#bookDesc_iframe_wrapper")));
         // console.log(Object.getOwnPropertyNames($("#bookDesc_iframe_wrapper")[0]));
         // console.log($("#bookDesc_iframe_wrapper")[0].parent);
-        
+
+        // // Using meta tag description
+        // let desc = $("meta[name='description']")[0].attribs.content;
+        // console.log(desc);
+
+        let desc = $($('noscript:nth-child(2)')[0].childNodes[0].data).text().replace(/[\n\t]/g,'');
         let price = parseFloat($('#buyBoxInner').find("span.a-text-strike").text().replace(/\$/g,''));
         let productDetails =  $('#productDetailsTable').find('li');       
         let id = productDetails[3].children[1].data.replace(/^[ \t(]+/g,'');
@@ -48,14 +50,14 @@ axios.get(uri)
         let weight = productDetails[6].children[1].data.replace(/^[ \t(]+|[()]/g,'');
         let imageURLs = Object.keys(JSON.parse($("#imgBlkFront").attr("data-a-dynamic-image")));
 
-        // let book = new Book(id, name, price, desc, dimensions, imageURLs, weight);
-        // // fs.appendFileSync('amazon.txt', book);
-        // fs.writeFile("amazon.json", JSON.stringify({"product":book}), {encoding:"utf8"}, function(err) {
-        //   if(err) {
-        //       console.log(err);
-        //   } else {
-        //       console.log("The file was saved!");
-        //   }
-        // });
+        let book = new Book(id, name, price, desc, dimensions, imageURLs, weight);
+        // fs.appendFileSync('amazon.txt', book);
+        fs.writeFile("amazon.txt", JSON.stringify({"product":book}), {encoding:"utf8"}, function(err) {
+          if(err) {
+              console.log(err);
+          } else {
+              console.log("The file was saved!");
+          }
+        });
       }
     }, (error) => console.log(err) );
