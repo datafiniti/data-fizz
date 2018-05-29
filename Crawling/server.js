c3e140bd30c97434ff92321dad74fb5a3414bf54
jesseflores512 <jesseflores512@gmail.com>
2018-05-29 06:10:57 -0500

    nightmare working. fixed promise

--- Crawling/server.js
+++ Crawling/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const app = express();
 const cheerio = require('cheerio');
 const Nightmare = require('nightmare');
 const nightmare = Nightmare({ show: true });
-var fs = require('fs');
-const app = express();
+const fs = require('fs');
 
+
+//starting url
 const url = 'https://www.amazon.com/';
+//empty array to save urls
 let urls = [];
+
 //using nightmare to go to desired book page listing
 nightmare
   .goto(url)
@@ -14,64 +18,77 @@ nightmare
   .click('#a-page > div.a-container.fsdContainer.fsdFullWidthImage > div > div:nth-child(3) > div:nth-child(3) > div > a:nth-child(1)')
   .insert('#twotabsearchtextbox', 'batman')
   .click('#nav-search > form > div.nav-right > div > input')
-  .wait(2000)
+  .wait(2500)
   .click('#leftNavContainer > ul:nth-child(27) > div > li:nth-child(1) > span > a')
-  .wait(2000)
+  .wait(2500)
 
+//evaluate listing page with cheerio
   .evaluate(() => document.querySelector('body').outerHTML)
-    .then(function (html) {
-      const $ = cheerio.load(html);
+    .then((html) => {
+      let $ = cheerio.load(html);
 
+// pull all href links from products on search page, and push to urls array
       $('#atfResults > ul > li > div > div > div> div > div > div > a.s-access-detail-page').each(function(){
-          var link=$(this).attr('href')
-          urls.push(link);
+        let link=$(this).attr('href')
+        urls.push(link);
         })
-        urls.length=9;
-        urls.reduce(function(accumulator, url) {
-          return accumulator.then(function(results) {
+        urls.length=10;
+
+//run for each url in array to scrape data
+        urls.reduce((accumulator, url) => {
+          return accumulator.then((results) => {
+
             return nightmare.goto(url)
-              .wait(2500)
-              .title()
-              .then(function(result){
+              .wait('body')
+              .evaluate(() => {
+
+//grabbing elements with querySelector and saving to variables
+              let name = document.querySelector('#productTitle').innerText;
+
+              let img = document.querySelector('#imgBlkFront').src;
+
+              let description = document.querySelectorAll('noscript')[1].innerText;
+              let exclude = /[^<>&.*?;()/]\/|<div>|<i>|<em>|<b>|<br>|&mdash;|/g;
+              description = description.replace(exclude, '');
+
+              let priceContainer = document.querySelector("#buyNewSection");
+              let price = priceContainer.querySelector("div.inlineBlock-display > span.a-text-normal").innerText;
+
+              let weight = document.querySelector("#productDetailsTable > tbody > tr > td > div > ul > li:nth-child(8)").innerText.substring(17, 29).replace('(', "");
+
+              let dimension =  document.querySelector("#productDetailsTable > tbody > tr > td > div > ul > li:nth-child(7)").innerText.substring(20);
+
+//return all variables into object
+              return {
+                "name": name,
+                "description": description,
+                "imageUrl": img,
+                "price": price,
+                "dimension": dimension,
+                "weight": weight
+                }
+              })
+              .then((result) => {
                 results.push(result);
                 return results;
-                      });
-                nightmare.catch(error => { return someFunction(nightmare); })
-                  });
-            }, Promise.resolve([])).then(function(results){
-                console.dir(results);
-            });
+              });
+          })
+//push all objects into promise.resolve
+        }, Promise.resolve([])).then((results) => {
+            results = { "product" : results }
+              //write into json file
+            fs.writeFile('output.json', JSON.stringify(results, null, 4),(err) =>{
+              console.log('File successfully written! - Check your project directory for the output.json file');
+
 
             })
+            //end nightmarejs
+            nightmare.then(() => nightmare.end())
+            })
+      })
 
-
-
-
-    //
-    //
-    //   $('#s-results-list-atf li')
-
-    //   fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-    //
-    //   console.log('File successfully written! - Check your project directory for the output.json file');
-    //
-
-
-
-  // .catch(function (error) {
-  // console.error('Error:', error);
-  // });
-  // }
-
-
+//server connection
 app.listen(3000, (err) => {
   if (err) throw err;
   console.log('Magic happens on port 3000');
 });
-
-//select ul containing products
-//maybe use .map() or .each() to select 1st ten selectors then
-//write function under selector that uses nightmare to click on each product based on 'id result'
-//in the function once navigated to page, collect data using selectors then assign to json variable
-
-//also try replacing type() with insert() for faster results
