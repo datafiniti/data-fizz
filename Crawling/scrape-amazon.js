0a5ce0134c46c68cdcd01422bec32cb40e5a6c02
njang <neo.w.jang@gmail.com>
2018-06-01 13:33:14 -0500

    Refine scraper to catch errors

--- Crawling/scrape-amazon.js
+++ Crawling/scrape-amazon.js
@@ -2,8 +2,36 @@ let axios = require('axios');
 let cheerio = require('cheerio');
 let fs = require('fs'); 
 
+class Product {
+  constructor(id, name, price, description, dimensions, imageURLs, weight, sourceURL) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.dimensions = dimensions;
+    this.imageURLs = imageURLs;
+    this.weight = weight;
+    this.sourceURL = sourceURL;
+  }    
+}
+
 const startingURL = "https://www.amazon.com/" // Start from Amazon homepage.
 let bookList = [];
+
+const retrieve = (uri) => {
+  axios.get(uri).then(
+    (response) => {
+      if (response.status === 200) {
+        console.log(`URI retrieved: ${uri}`)
+        return cheerio.load(response.data);
+        resolve();
+      } 
+    }, 
+    (err) => {
+      console.log(err);
+    }
+  )
+}
 
 axios.get(startingURL)
   .then((response) => {
@@ -24,45 +52,92 @@ axios.get(startingURL)
         retrieveInfo(bookList[i])
       }
     }
-  }, (err) => console.log(err) )
-
-// const uri = "https://www.amazon.com/dp/1501180983"
-// const uri = "https://www.amazon.com/dp/1628600160"
-const url = "https://www.amazon.com/dp/1587676109"
-
-class Book {
-  constructor(id, name, price, description, dimensions, imageURLs, weight, sourceURL) {
-    this.id = id;
-    this.name = name;
-    this.price = price;
-    this.description = description;
-    this.dimensions = dimensions;
-    this.imageURLs = imageURLs;
-    this.weight = weight;
-    this.sourceURL = sourceURL;
-  }  
-}
+  }, (err) => console.log(err) 
+)
 
 const retrieveInfo = (uri) => {
   axios.get(uri).then((response) => {
     if(response.status === 200) {
-        let $ = cheerio.load(response.data);  // Store the response data.
-        let name = $('#productTitle').text(); // Retrieve the book title.
-        // let desc = $($('noscript:nth-child(2)')[0].childNodes[0].data).text().replace(/[\n\t]/g,'');  // Description of the book comes from the 2nd noscript element. Once found, text is extracted, and regex replace to remove carriage returns and leading/trailing empty spaces.
-        
-        let descHTML = $.createElement('div');
-        descHTML.innerHTML = $('noscript:nth-child(2)')[0].childNodes[0].data
-        let desc = descHTML.textContent.replace(/[\n\t]/g,'');  // Description of the book comes from the 2nd noscript element. Once found, text is extracted, and regex replace to remove carriage returns and leading/trailing empty spaces.
-        
-        let price = parseFloat($('#buyBoxInner').find("span.a-text-strike").text().replace(/\$/g,'')).toFixed(2); // List price of the product is found inside buybox, where the strikethrough is applied. Retain 2 decimal places for proper format.
-        let productDetails =  $('#productDetailsTable').find('li'); // For ASIN ID, dimensions and weight, first locate the product details section.
-        let id = productDetails[3].children[1].data.replace(/^[ \t(]+/g,'');  // From product details, 4th item is the ISBN-10 which matches the ASIN ID of the book.
-        let dimensions = productDetails[5].children[1].data.split('\n')[1].replace(/^[ \t]+/g,'');  // From product details, 6th item is the dimensions. Once found, clean up the text.
-        let weight = productDetails[6].children[1].data.replace(/^[ \t(]+|[()]/g,''); // From product details, 7th item is the shipping weight. Once found, clean up the text.
-        let imageURLs = Object.keys(JSON.parse($("#imgBlkFront").attr("data-a-dynamic-image")));  // Image URLs can be located from img with id="imgBlkFront".
+      let $ = cheerio.load(response.data);  // Store the response data.
+      
+      // let name = $('#productTitle').text(); // Retrieve the book title.
+      let infoName = $('#productTitle'); // Retrieve the book title.
+      let name = "";
+      if (infoName) {
+        name = infoName.text();
+      } else {
+        console.log(`Name not found for ${uri}`);
+      }
 
-        console.log(`Retrieving information on book id: ${id}`)
-        
+      // let desc = $($('noscript:nth-child(2)')[0].childNodes[0].data).text().replace(/[\n\t]/g,'');  // Description of the book comes from the 2nd noscript element. Once found, text is extracted, and regex replace to remove carriage returns and leading/trailing empty spaces.
+      let infoDesc = $($('noscript:nth-child(2)')[0].childNodes[0].data);  // Description of the book comes from the 2nd noscript element. Once found, text is extracted, and regex replace to remove carriage returns and leading/trailing empty spaces.
+      let desc = "Not found";
+      if (infoDesc.text()) {
+        desc = infoDesc.text().replace(/[\n\t]/g,'')
+      } else {
+        console.log(`Description not found for ${uri}`);
+      }
+      // let descHTML = $.createElement('div');
+      // descHTML.innerHTML = $('noscript:nth-child(2)')[0].childNodes[0].data
+      // let desc = descHTML.textContent.replace(/[\n\t]/g,'');  // Description of the book comes from the 2nd noscript element. Once found, text is extracted, and regex replace to remove carriage returns and leading/trailing empty spaces.
+      
+      // let price = parseFloat($('#buyBoxInner').find("span.a-text-strike").text().replace(/\$/g,'')).toFixed(2); // List price of the product is found inside buybox, where the strikethrough is applied. Retain 2 decimal places for proper format.
+      let infoPrice = $('#buyBoxInner').find("span.a-text-strike");
+      let price = 0;
+      if (infoPrice.text()) {
+        price = parseFloat(infoPrice.text().replace(/\$/g,'')).toFixed(2);
+      } else {
+        console.log(`Price not found for ${uri}`);
+      }
+      
+      let productDetails =  $('#productDetailsTable').find('li'); // For ASIN ID, dimensions and weight, first locate the product details section.
+      let id = 0; let dimensions = "Not found"; let weight = "Not found";
+      if (productDetails[3].children[1].data) {
+        id = productDetails[3].children[1].data.replace(/^[ \t(]+/g,'');  // From product details, 4th item is the ISBN-10 which matches the ASIN ID of the book.
+      } else {
+        console.log(`ID not found for ${uri}`);
+      }
+      if (productDetails[5].children[1].data.split('\n')[1]) {
+        dimensions = productDetails[5].children[1].data.split('\n')[1].replace(/^[ \t]+/g,'');  // From product details, 6th item is the dimensions. Once found, clean up the text.
+      } else {
+        console.log(`Dimensions not found for ${uri}`);
+      }
+      if (productDetails[6].children[1].data) {
+        weight = productDetails[6].children[1].data.replace(/^[ \t(]+|[()]/g,''); // From product details, 7th item is the shipping weight. Once found, clean up the text.
+      } else {
+        console.log(`Weight not found for ${uri}`);
+      }
+
+      // let id = productDetails[3].children[1].data.replace(/^[ \t(]+/g,'');  // From product details, 4th item is the ISBN-10 which matches the ASIN ID of the book.
+      // let dimensions = productDetails[5].children[1].data.split('\n')[1].replace(/^[ \t]+/g,'');  // From product details, 6th item is the dimensions. Once found, clean up the text.
+      // let weight = productDetails[6].children[1].data.replace(/^[ \t(]+|[()]/g,''); // From product details, 7th item is the shipping weight. Once found, clean up the text.
+      let imageURLs = Object.keys(JSON.parse($("#imgBlkFront").attr("data-a-dynamic-image")));  // Image URLs can be located from img with id="imgBlkFront".
+
+      console.log(`Retrieving information on book id: ${id}`)
+
+      let book = new Product(id, name, price, desc, dimensions, imageURLs, weight, uri);
+      fs.writeFile(`books/book_${id}.txt`, JSON.stringify({"product":book}), {encoding:"utf8"}, function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("The file was saved!");
+        }
+      }); 
+
+      
+    }
+  }, (err) => console.log(err) );
+}
+
+
+
+
+
+  
+
+// const uri = "https://www.amazon.com/dp/1501180983"
+// const uri = "https://www.amazon.com/dp/1628600160"
+// const url = "https://www.amazon.com/dp/1587676109"     
         // Below attempts to traverse into iframe turned out futile.
         // let desc = $('iframe#bookDesc_iframe');
         // let desc = $('#bookDesc_iframe_wrapper')[0]
@@ -83,17 +158,3 @@ const retrieveInfo = (uri) => {
         // // Using meta tag description
         // let desc = $("meta[name='description']")[0].attribs.content;
         // console.log(desc);
-
-        let book = new Book(id, name, price, desc, dimensions, imageURLs, weight, uri);
-        fs.writeFile(`books/book_${id}.txt`, JSON.stringify({"product":book}), {encoding:"utf8"}, function(err) {
-          if(err) {
-              console.log(err);
-          } else {
-              console.log("The file was saved!");
-          }
-        }); 
-
-       
-      }
-    }, (err) => console.log(err) );
-}
